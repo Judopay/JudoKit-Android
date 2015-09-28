fb9bd5188eca016af668a4bfc562294aed2a1b31
James Goodwin <james.goodwin@judopayments.com>
2015-09-28 15:52:33 +0100

    Add Address.Builder

--- judo-rest/src/main/java/com/judopay/customer/Address.java
+++ judo-rest/src/main/java/com/judopay/customer/Address.java
@@ -12,4 +12,28 @@ public class Address {
     public String getPostCode() {
         return postCode;
     }
+
+    public static class Builder {
+
+        private Address address;
+
+        public Builder() {
+            this.address = new Address();
+        }
+
+        public Builder setCountryCode(long countryCode) {
+            address.countryCode = countryCode;
+            return this;
+        }
+
+        public Builder setPostCode(String postCode) {
+            address.postCode = postCode;
+            return this;
+        }
+
+        public Address build() {
+            return address;
+        }
+    }
+
 }
