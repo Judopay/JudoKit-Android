8d007659b723ce2d784bd3fe290d778db54fac15
James Goodwin <james.goodwin@judopayments.com>
2015-09-28 16:01:59 +0100

    Save API token & secret in SharedPreferences for access when performing requests

--- judo-rest/src/main/java/com/judopay/JudoPay.java
+++ judo-rest/src/main/java/com/judopay/JudoPay.java
@@ -1,13 +1,28 @@
 package com.judopay;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 
 public class JudoPay {
 
     private static Context context;
 
+    private static final String SHARED_PREFS = "JudoPrefs";
+    private static final String API_TOKEN = "JudoApiToken";
+    private static final String API_SECRET = "JudoApiSecret";
+
     public void initialise(Context context, String apiToken, String apiSecret) {
-        context = context.getApplicationContext();
+        JudoPay.context = context.getApplicationContext();
+        saveApiCredentials(apiToken, apiSecret);
+    }
+
+    private void saveApiCredentials(String apiToken, String apiSecret) {
+        SharedPreferences sharedPreferences = context.getSharedPreferences(SHARED_PREFS, Context.MODE_PRIVATE);
+
+        sharedPreferences.edit()
+                .putString(API_TOKEN, apiToken)
+                .putString(API_SECRET, apiSecret)
+                .apply();
     }
 
     public static Context getContext() {
