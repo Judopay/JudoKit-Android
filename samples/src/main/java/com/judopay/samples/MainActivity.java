be5df3b79fe0463e2cb8958b2874184c50d608d9
James Goodwin <james.goodwin@judopayments.com>
2015-09-29 09:04:10 +0100

    Sample call of Payment request

--- samples/src/main/java/com/judopay/samples/MainActivity.java
+++ samples/src/main/java/com/judopay/samples/MainActivity.java
@@ -1,7 +1,15 @@
 package com.judopay.samples;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+import com.judopay.Client;
+import com.judopay.JudoPay;
+import com.judopay.customer.Address;
+import com.judopay.customer.Location;
+import com.judopay.payment.PaymentService;
+import com.judopay.payment.Transaction;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +17,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(com.judopay.samples.R.layout.activity_main);
+
+        JudoPay judoPay = new JudoPay();
+        judoPay.initialise(this, "823Eja2fEM6E9NAE", "382df6f458294f49f02f073e8f356f8983e2460631ea1b4c8ed4c3ee502dcbe6");
+
+        final PaymentService paymentService = new PaymentService();
+
+        findViewById(R.id.payment_button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Transaction.Builder builder = new Transaction.Builder()
+                        .setAmount("9.99")
+                        .setCardAddress(new Address.Builder()
+                                .setPostCode("SW16 6AT")
+                                .build())
+                        .setClientDetails(new Client())
+                        .setConsumerLocation(new Location())
+                        .setCardNumber("4976000000003436")
+                        .setCurrency("GBP")
+                        .setCv2("452")
+                        .setJudoId(100407196)
+                        .setYourConsumerReference("consumerRef")
+                        .setYourPaymentReference("paymentRef")
+                        .setExpiryDate("12/15");
+
+                paymentService.payment(builder.build());
+            }
+        });
     }
 
 }
