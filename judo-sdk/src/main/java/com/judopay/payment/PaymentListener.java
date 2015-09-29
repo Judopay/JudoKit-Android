e940feedf08f844262c3d66d207a929b0065e84d
jamesgoodwin <james.goodwin@judopayment.com>
2015-09-29 16:37:28 +0100

    Add callback methods to PaymentListener

--- judo-sdk/src/main/java/com/judopay/payment/PaymentListener.java
+++ judo-sdk/src/main/java/com/judopay/payment/PaymentListener.java
@@ -1,5 +1,11 @@
 package com.judopay.payment;
 
+import retrofit.Response;
+
 public interface PaymentListener {
+
+    void onPaymentSuccess(Response<PaymentResponse> response);
+
+    void onFailure(Throwable t);
 
 }
