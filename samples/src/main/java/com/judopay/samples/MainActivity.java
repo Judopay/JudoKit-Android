a7ef77fd15b3b1fbe7a7403192d9b3fc7c689d46
jamesgoodwin <james.goodwin@judopayment.com>
2015-09-29 15:47:51 +0100

    Start PaymentActivity instead of calling PaymentService directly

--- samples/src/main/java/com/judopay/samples/MainActivity.java
+++ samples/src/main/java/com/judopay/samples/MainActivity.java
@@ -1,49 +1,34 @@
 package com.judopay.samples;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
-import com.judopay.Client;
+import com.judopay.Consumer;
 import com.judopay.JudoPay;
-import com.judopay.customer.Address;
-import com.judopay.customer.Location;
-import com.judopay.payment.PaymentService;
-import com.judopay.payment.Transaction;
+import com.judopay.payment.Payment;
+import com.judopay.payment.PaymentActivity;
+
+import static com.judopay.payment.PaymentActivity.JUDO_PAYMENT;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(com.judopay.samples.R.layout.activity_main);
 
         JudoPay judoPay = new JudoPay();
         judoPay.initialise(this, "823Eja2fEM6E9NAE", "382df6f458294f49f02f073e8f356f8983e2460631ea1b4c8ed4c3ee502dcbe6");
 
-        final PaymentService paymentService = new PaymentService();
+        Intent intent = new Intent(this, PaymentActivity.class);
+        intent.putExtra(JUDO_PAYMENT, new Payment.Builder()
+                .setJudoId(100407196)
+                .setCurrency("GBP")
+                .setConsumer(new Consumer())
+                .setPaymentRef("paymentRef")
+                .build());
 
-        findViewById(R.id.payment_button).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Transaction.Builder builder = new Transaction.Builder()
-                        .setAmount("9.99")
-                        .setCardAddress(new Address.Builder()
-                                .setPostCode("SW16 6AT")
-                                .build())
-                        .setClientDetails(new Client())
-                        .setConsumerLocation(new Location())
-                        .setCardNumber("4976000000003436")
-                        .setCurrency("GBP")
-                        .setCv2("452")
-                        .setJudoId(100407196)
-                        .setYourConsumerReference("consumerRef")
-                        .setYourPaymentReference("paymentRef")
-                        .setExpiryDate("12/15");
-
-                paymentService.payment(builder.build());
-            }
-        });
+        startActivity(intent);
     }
 
 }
