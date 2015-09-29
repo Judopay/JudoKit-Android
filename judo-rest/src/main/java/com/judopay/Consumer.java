78f4fdbe41befc746247f6a2138084298c7808c6
jamesgoodwin <james.goodwin@judopayment.com>
2015-09-29 15:41:46 +0100

    Make Consumer parcelable

--- judo-rest/src/main/java/com/judopay/Consumer.java
+++ judo-rest/src/main/java/com/judopay/Consumer.java
@@ -1,23 +1,39 @@
 package com.judopay;
 
-public class Consumer {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Consumer implements Parcelable {
 
     private String consumerToken;
     private String yourConsumerReference;
 
-    public String getConsumerToken() {
-        return consumerToken;
+    public Consumer() { }
+
+    private Consumer(Parcel in) {
+        this.consumerToken = in.readString();
+        this.yourConsumerReference = in.readString();
     }
 
-    public String getYourConsumerReference() {
-        return yourConsumerReference;
+    public static final Parcelable.Creator<Consumer> CREATOR = new Parcelable.Creator<Consumer>() {
+        public Consumer createFromParcel(Parcel source) {
+            return new Consumer(source);
+        }
+
+        public Consumer[] newArray(int size) {
+            return new Consumer[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
     }
 
     @Override
-    public String toString() {
-        return "Consumer{" +
-                "consumerToken='" + consumerToken + '\'' +
-                ", yourConsumerReference='" + yourConsumerReference + '\'' +
-                '}';
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(this.consumerToken);
+        dest.writeString(this.yourConsumerReference);
     }
+
 }
