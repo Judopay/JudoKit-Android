05499f1c6c3a364b17edb4028872b2096768c6c6
jamesgoodwin <james.goodwin@judopayment.com>
2015-09-30 15:47:11 +0100

    Factory method for creating Retrofit REST client

---
+++ judo-rest/src/main/java/com/judopay/arch/api/RetrofitFactory.java
@@ -0,0 +1,53 @@
+package com.judopay.arch.api;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.judopay.JudoPay;
+import com.judopay.auth.AuthenticationInterceptor;
+import com.judopay.auth.AuthorizationEncoder;
+import com.squareup.okhttp.CertificatePinner;
+import com.squareup.okhttp.OkHttpClient;
+
+import java.util.Date;
+
+import retrofit.GsonConverterFactory;
+import retrofit.Retrofit;
+import retrofit.RxJavaCallAdapterFactory;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+
+public class RetrofitFactory {
+
+    public static Retrofit get() {
+        AuthorizationEncoder authorizationEncoder = new AuthorizationEncoder(JudoPay.getContext());
+        AuthenticationInterceptor interceptor = new AuthenticationInterceptor(authorizationEncoder);
+
+        OkHttpClient client = new OkHttpClient();
+
+        client.setConnectTimeout(30, SECONDS);
+        client.setReadTimeout(30, SECONDS);
+        client.setWriteTimeout(30, SECONDS);
+
+        client.setCertificatePinner(new CertificatePinner.Builder()
+                .add("partnerapi.judopay-sandbox.com", "sha1/SSAG1hz7m8LI/eapL/SSpd5o564=")
+                .add("partnerapi.judopay-sandbox.com", "sha1/o5OZxATDsgmwgcIfIWIneMJ0jkw=")
+                .build());
+
+        client.interceptors()
+                .add(interceptor);
+
+        Gson gson = new GsonBuilder()
+                .registerTypeAdapter(Date.class, new DateJsonDeserializer())
+                .create();
+
+        GsonConverterFactory converterFactory = GsonConverterFactory.create(gson);
+
+        return new Retrofit.Builder()
+                .addConverterFactory(converterFactory)
+                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+                .baseUrl(JudoPay.getBaseUrl())
+                .client(client)
+                .build();
+    }
+
+}
