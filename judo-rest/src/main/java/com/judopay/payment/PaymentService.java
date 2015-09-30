cd9e1206d60b7f29c361ffb4bb2f0649aaca5e20
jamesgoodwin <james.goodwin@judopayment.com>
2015-09-30 15:47:39 +0100

    Use RetrofitFactory when creating API service endpoint

--- judo-rest/src/main/java/com/judopay/payment/PaymentService.java
+++ judo-rest/src/main/java/com/judopay/payment/PaymentService.java
@@ -1,19 +1,9 @@
 package com.judopay.payment;
 
-import com.google.gson.GsonBuilder;
-import com.judopay.JudoPay;
-import com.judopay.auth.AuthenticationInterceptor;
-import com.judopay.auth.AuthorizationEncoder;
-import com.judopay.util.DateJsonDeserializer;
-import com.squareup.okhttp.OkHttpClient;
-
-import java.util.Date;
+import com.judopay.arch.api.RetrofitFactory;
 
 import retrofit.Call;
 import retrofit.Callback;
-import retrofit.GsonConverterFactory;
-import retrofit.Retrofit;
-import retrofit.RxJavaCallAdapterFactory;
 
 public class PaymentService {
 
@@ -24,24 +14,7 @@ public class PaymentService {
     }
 
     public PaymentService() {
-        AuthorizationEncoder authorizationEncoder = new AuthorizationEncoder(JudoPay.getContext());
-        AuthenticationInterceptor interceptor = new AuthenticationInterceptor(authorizationEncoder);
-
-        OkHttpClient client = new OkHttpClient();
-        client.interceptors()
-                .add(interceptor);
-
-        GsonConverterFactory converterFactory = GsonConverterFactory.create(new GsonBuilder()
-                .registerTypeAdapter(Date.class, new DateJsonDeserializer())
-                .create());
-
-        this.paymentsApiService = new Retrofit.Builder()
-                .addConverterFactory(converterFactory)
-                .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
-                .baseUrl(JudoPay.getBaseUrl())
-                .client(client)
-                .build()
-                .create(PaymentsApiService.class);
+        this.paymentsApiService = RetrofitFactory.get().create(PaymentsApiService.class);
     }
 
     public void payment(Transaction transaction, Callback<PaymentResponse> callback) {
