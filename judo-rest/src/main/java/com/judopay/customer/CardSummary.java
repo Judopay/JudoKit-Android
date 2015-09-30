ea3d09373cb12c6bb5d29cd2128f1d3ac4313dd0
jamesgoodwin <james.goodwin@judopayment.com>
2015-09-30 10:35:06 +0100

    Rename class to reflect that it is a summary of a Card, not the full details

---
+++ judo-rest/src/main/java/com/judopay/customer/CardSummary.java
@@ -0,0 +1,43 @@
+package com.judopay.customer;
+
+import com.google.gson.annotations.SerializedName;
+
+public class CardSummary {
+
+    private String endDate;
+
+    @SerializedName("cardLastfour")
+    private String lastFour;
+
+    @SerializedName("cardToken")
+    private String token;
+
+    @SerializedName("cardType")
+    private long type;
+
+    public String getLastFour() {
+        return lastFour;
+    }
+
+    public String getEndDate() {
+        return endDate;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public long getType() {
+        return type;
+    }
+
+    @Override
+    public String toString() {
+        return "CardSummary{" +
+                "endDate='" + endDate + '\'' +
+                ", lastFour='" + lastFour + '\'' +
+                ", token='" + token + '\'' +
+                ", type=" + type +
+                '}';
+    }
+}
