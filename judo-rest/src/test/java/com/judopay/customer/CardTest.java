c7776f69d0cd9416354fb748f968440e53e846e4
jamesgoodwin <james.goodwin@judopayment.com>
2015-10-01 10:40:10 +0100

    Pass null for CardAddress as not needed in unit test

--- judo-rest/src/test/java/com/judopay/customer/CardTest.java
+++ judo-rest/src/test/java/com/judopay/customer/CardTest.java
@@ -9,82 +9,82 @@ public class CardTest {
 
     @Test
     public void shouldHaveInvalidLuhn() {
-        Card card = new Card("1234567890123456", null, new CardDate(1, 99), null);
+        Card card = new Card("1234567890123456", null, null, new CardDate(1, 99), null);
         assertThat(card.isLuhnValid(), is(false));
     }
 
     @Test
     public void shouldHaveValidLuhn() {
-        Card card = new Card("4976000000003436", null, new CardDate(1, 99), null);
+        Card card = new Card("4976000000003436", null, null, new CardDate(1, 99), null);
         assertThat(card.isLuhnValid(), is(true));
     }
 
     @Test
     public void shouldHaveValidStartDate() {
-        Card card = new Card("4976000000003436", new CardDate(1, 12), null, null);
+        Card card = new Card("4976000000003436", null, new CardDate(1, 12), null, null);
         assertThat(card.isStartDateValid(), is(true));
     }
 
     @Test
     public void shouldHaveInvalidStartDate() {
-        Card card = new Card("4976000000003436", new CardDate(1, 99), null, null);
+        Card card = new Card("4976000000003436", null, new CardDate(1, 99), null, null);
         assertThat(card.isStartDateValid(), is(false));
     }
 
     @Test
     public void shouldHaveValidExpiryDate() {
-        Card card = new Card("4976000000003436", null, new CardDate(1, 99), null);
+        Card card = new Card("4976000000003436", null, null, new CardDate(1, 99), null);
         assertThat(card.isExpiryDateValid(), is(true));
     }
 
     @Test
     public void shouldMatchAmexCardType() {
-        Card card = new Card("340000000000009", null, null, null);
+        Card card = new Card("340000000000009", null, null, null, null);
         assertThat(card.getType(), is(Card.Type.AMEX));
     }
 
     @Test
     public void shouldMatchVisaCardType() {
-        Card card = new Card("4111111111111111", null, null, null);
+        Card card = new Card("4111111111111111", null, null, null, null);
         assertThat(card.getType(), is(Card.Type.VISA));
     }
 
     @Test
     public void shouldMatchMastercardCardType() {
-        Card card = new Card("5500000000000004", null, null, null);
+        Card card = new Card("5500000000000004", null, null, null, null);
         assertThat(card.getType(), is(Card.Type.MASTERCARD));
     }
 
     @Test
     public void shouldMatchMaestroCardType() {
-        Card card = new Card("6759649826438453", null, null, null);
+        Card card = new Card("6759649826438453", null, null, null, null);
         assertThat(card.getType(), is(Card.Type.MAESTRO));
     }
 
     @Test
     public void shouldHaveValidCidvAmexCard() {
-        Card card = new Card("340000000000009", null, null, "1234");
+        Card card = new Card("340000000000009", null, null, null, "1234");
 
         assertThat(card.isCvvValid(), is(true));
     }
 
     @Test
     public void shouldHaveInvalidCidvAmexCard() {
-        Card card = new Card("340000000000009", null, null, "10001");
+        Card card = new Card("340000000000009", null, null, null, "10001");
 
         assertThat(card.isCvvValid(), is(false));
     }
 
     @Test
     public void shouldHaveValidCv2VisaCard() {
-        Card card = new Card("4111111111111111", null, null, "0123");
+        Card card = new Card("4111111111111111", null, null, null, "0123");
 
         assertThat(card.isCvvValid(), is(true));
     }
 
     @Test
     public void shouldHaveInvalidCv2VisaCard() {
-        Card card = new Card("4111111111111111", null, null, "1001");
+        Card card = new Card("4111111111111111", null, null, null, "1001");
 
         assertThat(card.isCvvValid(), is(false));
     }
