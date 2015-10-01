8af5fb5cad599ee32c819a520e25b766f44e00c8
jamesgoodwin <james.goodwin@judopayment.com>
2015-10-01 11:05:36 +0100

    Move payment API call with PaymentActivity (previously inside fragment)

--- judo-sdk/src/main/java/com/judopay/payment/PaymentActivity.java
+++ judo-sdk/src/main/java/com/judopay/payment/PaymentActivity.java
@@ -6,23 +6,31 @@ import android.os.Parcelable;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 
+import com.judopay.Client;
 import com.judopay.JudoPay;
+import com.judopay.customer.Address;
+import com.judopay.customer.Card;
+import com.judopay.customer.Location;
 
+import retrofit.Callback;
 import retrofit.Response;
 
-public class PaymentActivity extends AppCompatActivity implements PaymentListener {
+public class PaymentActivity extends AppCompatActivity implements PaymentFormListener {
 
     public static final String JUDO_PAYMENT = "Judo-Payment";
+
+    private PaymentService paymentService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         validateParcelableExtra(JUDO_PAYMENT);
+        this.paymentService = new PaymentService();
 
         if (savedInstanceState == null) {
             Parcelable payment = getIntent().getParcelableExtra(JUDO_PAYMENT);
-            Fragment fragment = CardPaymentFragment.newInstance(payment, this);
+            Fragment fragment = CardFormFragment.newInstance(payment, this);
 
             getSupportFragmentManager()
                     .beginTransaction()
@@ -40,14 +48,36 @@ public class PaymentActivity extends AppCompatActivity implements PaymentListene
     }
 
     @Override
-    public void onPaymentSuccess(Response<PaymentResponse> response) {
-        setResult(JudoPay.SUCCESS, new Intent());
-        finish();
-    }
+    public void onSubmitCard(Card card) {
+        Payment payment = getIntent().getParcelableExtra(JUDO_PAYMENT);
 
-    @Override
-    public void onFailure(Throwable t) {
-        setResult(JudoPay.ERROR, new Intent());
+        Transaction.Builder builder = new Transaction.Builder()
+                .setAmount(String.valueOf(payment.getAmount()))
+                .setCardAddress(new Address.Builder()
+                        .setPostCode(card.getCardAddress().getPostcode())
+                        .build())
+                .setClientDetails(new Client())
+                .setConsumerLocation(new Location())
+                .setCardNumber(card.getCardNumber())
+                .setCurrency(payment.getCurrency())
+                .setCv2(card.getCv2())
+                .setJudoId(payment.getJudoId())
+                .setYourConsumerReference(payment.getConsumer().toString())
+                .setYourPaymentReference(payment.getPaymentRef())
+                .setExpiryDate(card.getExpiryDate());
+
+        paymentService.payment(builder.build(), new Callback<PaymentResponse>() {
+            @Override
+            public void onResponse(Response<PaymentResponse> response) {
+                setResult(JudoPay.SUCCESS, new Intent());
+                finish();
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+                setResult(JudoPay.ERROR, new Intent());
+            }
+        });
     }
 
 }
