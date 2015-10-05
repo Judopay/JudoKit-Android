b28dd3252e57186046f0a4e7916ba0d5cc04dc14
jamesgoodwin <james.goodwin@judopayment.com>
2015-10-05 09:22:31 +0100

    Add countryCode field for AVS checks Add builder for easier object construction

--- judo-rest/src/main/java/com/judopay/customer/CardAddress.java
+++ judo-rest/src/main/java/com/judopay/customer/CardAddress.java
@@ -2,13 +2,39 @@ package com.judopay.customer;
 
 public class CardAddress {
 
+    private int countryCode;
     private String postcode;
-
-    public CardAddress(String postcode) {
-        this.postcode = postcode;
-    }
 
     public String getPostcode() {
         return postcode;
+    }
+
+    public int getCountryCode() {
+        return countryCode;
+    }
+
+    private CardAddress() { }
+
+    public static class Builder {
+
+        private CardAddress cardAddress;
+
+        public Builder() {
+            this.cardAddress = new CardAddress();
+        }
+
+        public Builder setCountryCode(int countryCode) {
+            cardAddress.countryCode = countryCode;
+            return this;
+        }
+
+        public Builder setPostcode(String postcode) {
+            cardAddress.postcode = postcode;
+            return this;
+        }
+
+        public CardAddress build() {
+            return cardAddress;
+        }
     }
 }
