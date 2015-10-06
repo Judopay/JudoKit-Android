dc4fd921beb17a8fa8c644fd1c7ac442ffaaf164
jamesgoodwin <james.goodwin@judopayment.com>
2015-10-06 17:53:37 +0100

    Create JudoPayApi for representing API configuration options and settings

---
+++ judo-data/src/main/java/com/judopay/JudoPayApi.java
@@ -0,0 +1,67 @@
+package com.judopay;
+
+public class JudoPayApi {
+
+    private int judoId;
+    private String apiToken;
+    private String apiSecret;
+    private int apiEnvironment;
+
+    private boolean sslPinningEnabled;
+    private boolean avsEnabled;
+
+    public JudoPayApi(String apiToken, String apiSecret, int apiEnvironment) {
+        this.apiToken = apiToken;
+        this.apiSecret = apiSecret;
+        this.apiEnvironment = apiEnvironment;
+    }
+
+    public int getJudoId() {
+        return judoId;
+    }
+
+    public void setJudoId(int judoId) {
+        this.judoId = judoId;
+    }
+
+    public String getApiToken() {
+        return apiToken;
+    }
+
+    public void setApiToken(String apiToken) {
+        this.apiToken = apiToken;
+    }
+
+    public String getApiSecret() {
+        return apiSecret;
+    }
+
+    public void setApiSecret(String apiSecret) {
+        this.apiSecret = apiSecret;
+    }
+
+    public int getApiEnvironment() {
+        return apiEnvironment;
+    }
+
+    public void setApiEnvironment(int apiEnvironment) {
+        this.apiEnvironment = apiEnvironment;
+    }
+
+    public boolean isSslPinningEnabled() {
+        return sslPinningEnabled;
+    }
+
+    public void setSslPinningEnabled(boolean sslPinningEnabled) {
+        this.sslPinningEnabled = sslPinningEnabled;
+    }
+
+    public boolean isAvsEnabled() {
+        return avsEnabled;
+    }
+
+    public void setAvsEnabled(boolean avsEnabled) {
+        this.avsEnabled = avsEnabled;
+    }
+
+}
