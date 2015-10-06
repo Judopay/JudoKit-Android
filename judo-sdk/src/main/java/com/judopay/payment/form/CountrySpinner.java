631c9136eb15175a77a4eaa47618e8f46048f09b
jamesgoodwin <james.goodwin@judopayment.com>
2015-10-06 12:39:51 +0100

    Add listener for notifying when other country is selected

--- judo-sdk/src/main/java/com/judopay/payment/form/CountrySpinner.java
+++ judo-sdk/src/main/java/com/judopay/payment/form/CountrySpinner.java
@@ -4,6 +4,8 @@ import android.annotation.TargetApi;
 import android.content.Context;
 import android.os.Build;
 import android.util.AttributeSet;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Spinner;
 
 import com.judopay.customer.Country;
@@ -13,7 +15,14 @@ import java.util.List;
 
 public class CountrySpinner extends Spinner {
 
+    public static final String OTHER_COUNTRY = "Other";
     private CountrySpinnerAdapter adapter;
+    private Listener listener;
+
+    public interface Listener {
+        void onCountrySelected();
+        void onOtherCountrySelected();
+    }
 
     public CountrySpinner(Context context) {
         super(context);
@@ -38,7 +47,27 @@ public class CountrySpinner extends Spinner {
 
     private void initialise() {
         this.adapter = new CountrySpinnerAdapter(this.getContext(), android.R.layout.simple_list_item_1, getCountries());
+
         this.setAdapter(this.adapter);
+
+        super.setOnItemSelectedListener(new OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Country country = adapter.getItem(position);
+                if (listener != null) {
+                    if (OTHER_COUNTRY.equals(country.getDisplayName())) {
+                        listener.onOtherCountrySelected();
+                    } else {
+                        listener.onCountrySelected();
+                    }
+                }
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
     }
 
     private List<Country> getCountries() {
@@ -47,9 +76,13 @@ public class CountrySpinner extends Spinner {
         countries.add(new Country(826, "United Kingdom"));
         countries.add(new Country(840, "United States"));
         countries.add(new Country(124, "Canada"));
-        countries.add(new Country(0, "Other"));
+        countries.add(new Country(0, OTHER_COUNTRY));
 
         return countries;
+    }
+
+    public void setListener(Listener listener) {
+        this.listener = listener;
     }
 
     public Country getSelectedCountry() {
