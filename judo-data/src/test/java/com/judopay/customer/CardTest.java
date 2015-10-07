6d76c69035c700c39c23fac7f82691fe520383b5
jamesgoodwin <james.goodwin@judopayment.com>
2015-10-07 15:05:18 +0100

    Update CardTest to use new Card.Builder construction

--- judo-data/src/test/java/com/judopay/customer/CardTest.java
+++ judo-data/src/test/java/com/judopay/customer/CardTest.java
@@ -9,82 +9,121 @@ public class CardTest {
 
     @Test
     public void shouldHaveInvalidLuhn() {
-        Card card = new Card("1234567890123456", null, null, new CardDate(1, 99), null);
+        Card card = new Card.Builder()
+                .setCardNumber("1234567890123456")
+                .build();
+
         assertThat(card.isLuhnValid(), is(false));
     }
 
     @Test
     public void shouldHaveValidLuhn() {
-        Card card = new Card("4976000000003436", null, null, new CardDate(1, 99), null);
+        Card card = new Card.Builder()
+                .setCardNumber("4976000000003436")
+                .build();
+
         assertThat(card.isLuhnValid(), is(true));
     }
 
     @Test
     public void shouldHaveValidStartDate() {
-        Card card = new Card("4976000000003436", null, new CardDate(1, 12), null, null);
+        Card card = new Card.Builder()
+                .setStartDate(new CardDate(1, 12))
+                .build();
+
         assertThat(card.isStartDateValid(), is(true));
     }
 
     @Test
     public void shouldHaveInvalidStartDate() {
-        Card card = new Card("4976000000003436", null, new CardDate(1, 99), null, null);
+        Card card = new Card.Builder()
+                .setStartDate(new CardDate(1, 99))
+                .build();
+
         assertThat(card.isStartDateValid(), is(false));
     }
 
     @Test
     public void shouldHaveValidExpiryDate() {
-        Card card = new Card("4976000000003436", null, null, new CardDate(1, 99), null);
+        Card card = new Card.Builder()
+                .setExpiryDate(new CardDate(1, 99))
+                .build();
+
         assertThat(card.isExpiryDateValid(), is(true));
     }
 
     @Test
     public void shouldMatchAmexCardType() {
-        Card card = new Card("340000000000009", null, null, null, null);
+        Card card = new Card.Builder()
+                .setCardNumber("340000000000009")
+                .build();
+
         assertThat(card.getType(), is(CardType.AMEX));
     }
 
     @Test
     public void shouldMatchVisaCardType() {
-        Card card = new Card("4111111111111111", null, null, null, null);
+        Card card = new Card.Builder()
+                .setCardNumber("4111111111111111")
+                .build();
+
         assertThat(card.getType(), is(CardType.VISA));
     }
 
     @Test
     public void shouldMatchMastercardCardType() {
-        Card card = new Card("5500000000000004", null, null, null, null);
+        Card card = new Card.Builder()
+                .setCardNumber("5500000000000004")
+                .build();
+
         assertThat(card.getType(), is(CardType.MASTERCARD));
     }
 
     @Test
     public void shouldMatchMaestroCardType() {
-        Card card = new Card("6759649826438453", null, null, null, null);
+        Card card = new Card.Builder()
+                .setCardNumber("6759649826438453")
+                .build();
+
         assertThat(card.getType(), is(CardType.MAESTRO));
     }
 
     @Test
     public void shouldHaveValidCidvAmexCard() {
-        Card card = new Card("340000000000009", null, null, null, "1234");
+        Card card = new Card.Builder()
+                .setCardNumber("340000000000009")
+                .setCvv("1234")
+                .build();
 
         assertThat(card.isCvvValid(), is(true));
     }
 
     @Test
     public void shouldHaveInvalidCidvAmexCard() {
-        Card card = new Card("340000000000009", null, null, null, "10001");
+        Card card = new Card.Builder()
+                .setCardNumber("340000000000009")
+                .setCvv("10001")
+                .build();
 
         assertThat(card.isCvvValid(), is(false));
     }
 
     @Test
     public void shouldHaveValidCv2VisaCard() {
-        Card card = new Card("4111111111111111", null, null, null, "0123");
+        Card card = new Card.Builder()
+                .setCardNumber("4111111111111111")
+                .setCvv("0123")
+                .build();
 
         assertThat(card.isCvvValid(), is(true));
     }
 
     @Test
     public void shouldHaveInvalidCv2VisaCard() {
-        Card card = new Card("4111111111111111", null, null, null, "1001");
+        Card card = new Card.Builder()
+                .setCardNumber("4111111111111111")
+                .setCvv("1001")
+                .build();
 
         assertThat(card.isCvvValid(), is(false));
     }
