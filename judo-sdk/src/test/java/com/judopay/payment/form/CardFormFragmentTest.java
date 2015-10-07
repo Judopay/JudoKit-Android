41e3ac9f7c58f708a283a60db0936678b33b002e
jamesgoodwin <james.goodwin@judopayment.com>
2015-10-07 15:25:55 +0100

    Update CardFormFragmentTest to include tests for maestro field hide/showing

--- judo-sdk/src/test/java/com/judopay/payment/form/CardFormFragmentTest.java
+++ judo-sdk/src/test/java/com/judopay/payment/form/CardFormFragmentTest.java
@@ -3,6 +3,7 @@ package com.judopay.payment.form;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.View;
+import android.widget.EditText;
 
 import com.judopay.BuildConfig;
 import com.judopay.Consumer;
@@ -22,12 +23,12 @@ import static org.hamcrest.core.IsEqual.equalTo;
 import static org.robolectric.shadows.support.v4.SupportFragmentTestUtil.startFragment;
 
 @RunWith(RobolectricGradleTestRunner.class)
-@Config(constants = BuildConfig.class, sdk = 19)
+@Config(constants = BuildConfig.class, sdk = 21)
 public class CardFormFragmentTest {
 
     @Test
     public void shouldShowCountrySpinnerWhenAvsEnabled() {
-        JudoPay.setup(RuntimeEnvironment.application, "apiToken", "apiSecret", SANDBOX);
+        JudoPay.setup(RuntimeEnvironment.application, null, null, 0);
         JudoPay.setAvsEnabled(true);
 
         CardFormFragment fragment = getFragment();
@@ -41,7 +42,7 @@ public class CardFormFragmentTest {
 
     @Test
     public void shouldHideCountrySpinnerWhenAvsDisabled() {
-        JudoPay.setup(RuntimeEnvironment.application, "apiToken", "apiSecret", SANDBOX);
+        JudoPay.setup(RuntimeEnvironment.application, null, null, 0);
         JudoPay.setAvsEnabled(false);
 
         CardFormFragment fragment = getFragment();
@@ -51,6 +52,47 @@ public class CardFormFragmentTest {
         View countrySpinner = fragment.getView().findViewById(R.id.country_spinner);
 
         assertThat(countrySpinner.getVisibility(), equalTo(View.GONE));
+    }
+
+    @Test
+    public void shouldShowIssueNumberAndStartDateFieldsWhenMaestro() {
+        JudoPay.setup(RuntimeEnvironment.application, null, null, 0);
+
+        CardFormFragment fragment = getFragment();
+        startFragment(fragment);
+
+        View fragmentView = fragment.getView();
+
+        EditText cardNumberEditText = (EditText) fragmentView.findViewById(R.id.card_number_edit_text);
+
+        cardNumberEditText.setText("6759000000005462");
+
+        View issueNumberView = fragmentView.findViewById(R.id.issue_number_edit_text);
+        assertThat(issueNumberView.getVisibility(), equalTo(View.VISIBLE));
+
+        View startDateView = fragmentView.findViewById(R.id.start_date_edit_text);
+        assertThat(startDateView.getVisibility(), equalTo(View.VISIBLE));
+    }
+
+    @Test
+    public void shouldHideIssueNumberAndStartDateFieldsWhenNoLongerMaestro() {
+        JudoPay.setup(RuntimeEnvironment.application, null, null, 0);
+
+        CardFormFragment fragment = getFragment();
+        startFragment(fragment);
+
+        View fragmentView = fragment.getView();
+
+        EditText cardNumberEditText = (EditText) fragmentView.findViewById(R.id.card_number_edit_text);
+
+        cardNumberEditText.setText("6759000000005462");
+        cardNumberEditText.setText("4976000000003436");
+
+        View issueNumberView = fragmentView.findViewById(R.id.issue_number_edit_text);
+        assertThat(issueNumberView.getVisibility(), equalTo(View.GONE));
+
+        View startDateView = fragmentView.findViewById(R.id.start_date_edit_text);
+        assertThat(startDateView.getVisibility(), equalTo(View.GONE));
     }
 
     @NonNull
