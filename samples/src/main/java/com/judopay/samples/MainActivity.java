2804f48a0482d8011a6f7b7d951e02d184f76d2c
jamesgoodwin <james.goodwin@judopayment.com>
2015-10-08 14:17:32 +0100

    Remove AVS enabled setting from MainActivity (can be changed through SettingsActivity

--- samples/src/main/java/com/judopay/samples/MainActivity.java
+++ samples/src/main/java/com/judopay/samples/MainActivity.java
@@ -36,7 +36,6 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
 
         JudoPay.setup(this, "823Eja2fEM6E9NAE", "382df6f458294f49f02f073e8f356f8983e2460631ea1b4c8ed4c3ee502dcbe6", SANDBOX);
-        JudoPay.setAvsEnabled(true);
 
         initialiseView();
     }
