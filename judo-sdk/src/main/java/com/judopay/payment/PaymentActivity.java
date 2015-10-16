c624bd8d6ebc7d705538ac4afd882c036bf07329
jamesgoodwin <james.goodwin@judopayment.com>
2015-10-16 09:42:51 +0100

    Refactor of code from PaymentActivity into PaymentFragment

--- judo-sdk/src/main/java/com/judopay/payment/PaymentActivity.java
+++ judo-sdk/src/main/java/com/judopay/payment/PaymentActivity.java
@@ -4,57 +4,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Parcelable;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.ProgressBar;
 
-import com.judopay.Client;
 import com.judopay.JudoPay;
-import com.judopay.R;
-import com.judopay.customer.Address;
-import com.judopay.customer.Card;
-import com.judopay.customer.Location;
-import com.judopay.payment.form.PaymentFormFragment;
 
 import static com.judopay.JudoPay.EXTRA_PAYMENT;
 
-public class PaymentActivity extends AppCompatActivity implements PaymentFormListener, PaymentView {
+public class PaymentActivity extends AppCompatActivity implements PaymentListener {
 
     private static final String KEY_PAYMENT_FRAGMENT = "PaymentFormFragment";
-
-    private ProgressBar progressBar;
-    private PaymentFormFragment fragment;
-    private static PaymentPresenter presenter;
+    private PaymentFragment fragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_payment);
 
         validateParcelableExtra(EXTRA_PAYMENT);
 
-        this.progressBar = (ProgressBar) findViewById(R.id.progress_bar);
+        String title = getIntent().getStringExtra(Intent.EXTRA_TITLE);
+        this.setTitle(title != null ? title : "Payment");
 
-        if (savedInstanceState == null) {
-            presenter = new PaymentPresenter();
-
-            Parcelable payment = getIntent().getParcelableExtra(EXTRA_PAYMENT);
-
-            String title = getIntent().getStringExtra(Intent.EXTRA_TITLE);
-            this.setTitle(title != null ? title : "Payment");
-
-            this.fragment = PaymentFormFragment.newInstance(payment, this);
-
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .add(R.id.container, fragment)
-                    .commit();
-        } else {
-            this.fragment = (PaymentFormFragment) getSupportFragmentManager()
-                    .getFragment(savedInstanceState, KEY_PAYMENT_FRAGMENT);
-            this.fragment.setPaymentFormListener(this);
-        }
-
-        presenter.bindView(this);
+        showPaymentForm(savedInstanceState);
     }
 
     @Override
@@ -65,66 +34,25 @@ public class PaymentActivity extends AppCompatActivity implements PaymentFormLis
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        presenter.unbindView();
-    }
-
-    @Override
     public void onBackPressed() {
         super.onBackPressed();
         setResult(JudoPay.RESULT_CANCELED);
     }
 
-    @Override
-    public void onSubmit(Card card) {
-        Payment payment = getIntent().getParcelableExtra(EXTRA_PAYMENT);
+    private void showPaymentForm(Bundle savedInstanceState) {
+        if (savedInstanceState == null) {
+            Payment payment = getIntent().getParcelableExtra(EXTRA_PAYMENT);
+            this.fragment = PaymentFragment.newInstance(payment);
+            this.fragment.setPaymentListener(this);
 
-        Transaction.Builder builder = new Transaction.Builder()
-                .setAmount(String.valueOf(payment.getAmount()))
-                .setCardAddress(new Address.Builder()
-                        .setPostCode(card.getCardAddress().getPostcode())
-                        .build())
-                .setClientDetails(new Client())
-                .setConsumerLocation(new Location())
-                .setCardNumber(card.getCardNumber())
-                .setCurrency(payment.getCurrency())
-                .setCv2(card.getCv2())
-                .setJudoId(payment.getJudoId())
-                .setYourConsumerReference(payment.getConsumer().getYourConsumerReference())
-                .setYourPaymentReference(payment.getPaymentRef())
-                .setExpiryDate(card.getExpiryDate());
-
-        if(card.startDateAndIssueNumberRequired()) {
-            builder.setIssueNumber(card.getIssueNumber())
-                    .setStartDate(card.getStartDate());
-        }
-
-        presenter.performPayment(builder.build());
-    }
-
-    @Override
-    public void showLoading() {
-        progressBar.setVisibility(View.VISIBLE);
-    }
-
-    @Override
-    public void hideLoading() {
-        progressBar.setVisibility(View.GONE);
-    }
-
-    @Override
-    public void setViewModel(PaymentResponse paymentResponse) {
-        Intent intent = new Intent();
-        intent.putExtra(JudoPay.JUDO_RECEIPT, paymentResponse);
-
-        if(paymentResponse.isSuccess()) {
-            setResult(JudoPay.RESULT_PAYMENT_SUCCESS, intent);
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(android.R.id.content, fragment)
+                    .commit();
         } else {
-            setResult(JudoPay.RESULT_PAYMENT_DECLINED, intent);
+            this.fragment = (PaymentFragment) getSupportFragmentManager()
+                    .getFragment(savedInstanceState, KEY_PAYMENT_FRAGMENT);
         }
-
-        finish();
     }
 
     private void validateParcelableExtra(String extraName) {
@@ -133,6 +61,26 @@ public class PaymentActivity extends AppCompatActivity implements PaymentFormLis
             throw new IllegalArgumentException(String.format("%s extra must be supplied to %s", extraName,
                     this.getClass().getSimpleName()));
         }
+    }
+
+    @Override
+    public void onPaymentSuccess(PaymentResponse paymentResponse) {
+        Intent intent = new Intent();
+        intent.putExtra(JudoPay.JUDO_RECEIPT, paymentResponse);
+
+        setResult(JudoPay.RESULT_PAYMENT_SUCCESS, intent);
+
+        finish();
+    }
+
+    @Override
+    public void onPaymentDeclined(PaymentResponse paymentResponse) {
+        Intent intent = new Intent();
+        intent.putExtra(JudoPay.JUDO_RECEIPT, paymentResponse);
+
+        setResult(JudoPay.RESULT_PAYMENT_DECLINED, intent);
+
+        finish();
     }
 
 }
