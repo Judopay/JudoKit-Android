abd13175f1002f2124ddf3fc0d970c9d2b809a09
jamesgoodwin <james.goodwin@judopayment.com>
2015-10-19 18:01:01 +0100

    Remove Luhn validation from Card object, now accessed through CardNumber

--- judo-data/src/main/java/com/judopay/customer/Card.java
+++ judo-data/src/main/java/com/judopay/customer/Card.java
@@ -35,23 +35,6 @@ public class Card {
         return issueNumber;
     }
 
-    public boolean isLuhnValid() {
-        int sum = 0;
-        boolean alternate = false;
-        for (int i = cardNumber.length() - 1; i >= 0; i--) {
-            int n = Integer.parseInt(cardNumber.substring(i, i + 1));
-            if (alternate) {
-                n *= 2;
-                if (n > 9) {
-                    n = (n % 10) + 1;
-                }
-            }
-            sum += n;
-            alternate = !alternate;
-        }
-        return sum % 10 == 0;
-    }
-
     public boolean isStartDateValid() {
         return startDate.isPastDate();
     }
