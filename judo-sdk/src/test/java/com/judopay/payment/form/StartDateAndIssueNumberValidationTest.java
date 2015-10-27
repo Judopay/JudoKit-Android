b532268312224da43713fa4cf3277c590217ce99
jamesgoodwin <james.goodwin@judopayment.com>
2015-10-27 12:12:55 +0000

    Add new unit tests for testing when start date and issue number fields should be shown

--- judo-sdk/src/test/java/com/judopay/payment/form/StartDateAndIssueNumberValidationTest.java
+++ judo-sdk/src/test/java/com/judopay/payment/form/StartDateAndIssueNumberValidationTest.java
@@ -6,7 +6,7 @@ import com.judopay.customer.Country;
 import org.junit.Test;
 
 import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertThat;
 
 public class StartDateAndIssueNumberValidationTest {
 
@@ -20,7 +20,7 @@ public class StartDateAndIssueNumberValidationTest {
                 .setIssueNumber("01")
                 .setPostcode("")
                 .setCountry(new Country(0, Country.UNITED_KINGDOM))
-                .build(), true, true, true);
+                .build(), true);
 
         assertThat(startDateAndIssueNumberValidation.isStartDateEntryComplete(), is(true));
     }
@@ -28,14 +28,14 @@ public class StartDateAndIssueNumberValidationTest {
     @Test
     public void shouldHaveInvalidStartDate() {
         StartDateAndIssueNumberValidation startDateAndIssueNumberValidation = new StartDateAndIssueNumberValidation(new PaymentForm.Builder()
-                        .setCardNumber("1234567812345678")
-                        .setCvv("123")
-                        .setExpiryDate("01/35")
-                        .setStartDate("01/30")
-                        .setIssueNumber("01")
-                        .setPostcode("")
-                        .setCountry(new Country(0, Country.UNITED_KINGDOM))
-                        .build(), true, true, true);
+                .setCardNumber("1234567812345678")
+                .setCvv("123")
+                .setExpiryDate("01/35")
+                .setStartDate("01/30")
+                .setIssueNumber("01")
+                .setPostcode("")
+                .setCountry(new Country(0, Country.UNITED_KINGDOM))
+                .build(), true);
 
         assertThat(startDateAndIssueNumberValidation.isShowStartDateError(), is(true));
         assertThat(startDateAndIssueNumberValidation.getStartDateError(), is(R.string.error_check_date));
@@ -44,33 +44,67 @@ public class StartDateAndIssueNumberValidationTest {
     @Test
     public void shouldRequireIssueNumberAndStartDateWhenMaestroEntered() {
         StartDateAndIssueNumberValidation startDateAndIssueNumberValidation = new StartDateAndIssueNumberValidation(new PaymentForm.Builder()
-                        .setCardNumber("6759649826438453")
-                        .setCvv("123")
-                        .setExpiryDate("12/99")
-                        .setStartDate("")
-                        .setIssueNumber("")
-                        .setPostcode("")
-                        .setCountry(new Country(0, Country.UNITED_KINGDOM))
-                        .setMaestroSupported(true)
-                        .build(), true, true, true);
+                .setCardNumber("6759649826438453")
+                .setCvv("123")
+                .setExpiryDate("12/99")
+                .setStartDate("")
+                .setIssueNumber("")
+                .setPostcode("")
+                .setCountry(new Country(0, Country.UNITED_KINGDOM))
+                .setMaestroSupported(true)
+                .build(), true);
 
-        assertThat(startDateAndIssueNumberValidation.isIssueNumberAndStartDateRequired(), is(true));
+        assertThat(startDateAndIssueNumberValidation.isShowIssueNumberAndStartDate(), is(true));
     }
 
     @Test
     public void shouldNotRequireIssueNumberAndStartDateWhenMaestroEnteredButNotSupported() {
         StartDateAndIssueNumberValidation startDateAndIssueNumberValidation = new StartDateAndIssueNumberValidation(new PaymentForm.Builder()
-                        .setCardNumber("6759649826438453")
-                        .setCvv("123")
-                        .setExpiryDate("12/99")
+                .setCardNumber("6759649826438453")
+                .setCvv("123")
+                .setExpiryDate("12/99")
+                .setStartDate("")
+                .setIssueNumber("")
+                .setPostcode("")
+                .setCountry(new Country(0, Country.UNITED_KINGDOM))
+                .setMaestroSupported(false)
+                .build(), true);
+
+        assertThat(startDateAndIssueNumberValidation.isShowIssueNumberAndStartDate(), is(false));
+    }
+
+    @Test
+    public void shouldNotShowIssueNumberAndStartDateBeforeCardNumberEntered() {
+        StartDateAndIssueNumberValidation startDateAndIssueNumberValidation = new StartDateAndIssueNumberValidation(
+                new PaymentForm.Builder()
+                        .setCardNumber("")
+                        .setCvv("")
+                        .setExpiryDate("")
                         .setStartDate("")
                         .setIssueNumber("")
                         .setPostcode("")
                         .setCountry(new Country(0, Country.UNITED_KINGDOM))
-                        .setMaestroSupported(false)
-                        .build(), true, true, true);
+                        .setMaestroSupported(true)
+                        .build(), false);
 
-        assertThat(startDateAndIssueNumberValidation.isIssueNumberAndStartDateRequired(), is(false));
+        assertThat(startDateAndIssueNumberValidation.isShowIssueNumberAndStartDate(), is(false));
+    }
+
+    @Test
+    public void shouldShowIssueNumberAndStartDateWhenCardNumberEntered() {
+        StartDateAndIssueNumberValidation startDateAndIssueNumberValidation = new StartDateAndIssueNumberValidation(
+                new PaymentForm.Builder()
+                        .setCardNumber("6759649826438453")
+                        .setCvv("")
+                        .setExpiryDate("")
+                        .setStartDate("")
+                        .setIssueNumber("")
+                        .setPostcode("")
+                        .setCountry(new Country(0, Country.UNITED_KINGDOM))
+                        .setMaestroSupported(true)
+                        .build(), true);
+
+        assertThat(startDateAndIssueNumberValidation.isShowIssueNumberAndStartDate(), is(true));
     }
 
 }
