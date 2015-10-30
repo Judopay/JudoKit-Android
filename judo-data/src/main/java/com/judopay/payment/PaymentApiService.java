756ecdba5952f3ebb6bd63f7cb25ed8729325d3d
jamesgoodwin <james.goodwin@judopayment.com>
2015-10-30 14:47:31 +0000

    Add payment/receipt/collections endpoints to API Service

--- judo-data/src/main/java/com/judopay/payment/PaymentApiService.java
+++ judo-data/src/main/java/com/judopay/payment/PaymentApiService.java
@@ -2,11 +2,24 @@ package com.judopay.payment;
 
 import retrofit.http.Body;
 import retrofit.http.POST;
+import retrofit.http.PUT;
 import rx.Observable;
 
 public interface PaymentApiService {
 
     @POST("transactions/payments")
     Observable<PaymentResponse> payment(@Body Transaction transaction);
+
+    @PUT("transactions/{receiptId}")
+    Observable<PaymentResponse> threeDSecurePayment(String receiptId, @Body ThreeDSecureInfo threeDSecureInfo);
+
+    @POST("transactions/collections")
+    Observable<PaymentResponse> collection(@Body Collection collection);
+
+    @POST("transactions/refunds")
+    Observable<PaymentResponse> refund(@Body Refund refund);
+
+    @POST("transactions/preauths")
+    Observable<PaymentResponse> preAuth(@Body Transaction transaction);
 
 }
