30664f1337ee4251e32d4b17008602cf154831c4
jamesgoodwin <james.goodwin@judopayment.com>
2015-10-30 14:22:23 +0000

    New activity for performing pre-auth payments

---
+++ judo-sdk/src/main/java/com/judopay/payment/PreAuthActivity.java
@@ -0,0 +1,60 @@
+package com.judopay.payment;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+
+import com.judopay.JudoPay;
+
+import static com.judopay.JudoPay.EXTRA_PAYMENT;
+
+public class PreAuthActivity extends AppCompatActivity implements PaymentListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
+        if (savedInstanceState == null) {
+            Payment payment = getIntent().getParcelableExtra(EXTRA_PAYMENT);
+            PreAuthFragment fragment = PreAuthFragment.newInstance(payment, this);
+
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(android.R.id.content, fragment)
+                    .commit();
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        setResult(JudoPay.RESULT_CANCELED);
+    }
+
+    @Override
+    public void onPaymentSuccess(PaymentResponse paymentResponse) {
+        Intent intent = new Intent();
+        intent.putExtra(JudoPay.JUDO_RECEIPT, paymentResponse);
+
+        setResult(JudoPay.RESULT_PAYMENT_SUCCESS, intent);
+
+        finish();
+    }
+
+    @Override
+    public void onPaymentDeclined(PaymentResponse paymentResponse) {
+        Intent intent = new Intent();
+        intent.putExtra(JudoPay.JUDO_RECEIPT, paymentResponse);
+
+        setResult(JudoPay.RESULT_PAYMENT_DECLINED, intent);
+
+        finish();
+    }
+
+}
