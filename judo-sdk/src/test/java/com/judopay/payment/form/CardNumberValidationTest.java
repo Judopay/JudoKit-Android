9deb7356330894288125aac0f298979f152a1d96
jamesgoodwin <james.goodwin@judopayment.com>
2015-11-03 11:57:15 +0000

    Update tests to include new parameter for tokenCard

--- judo-sdk/src/test/java/com/judopay/payment/form/CardNumberValidationTest.java
+++ judo-sdk/src/test/java/com/judopay/payment/form/CardNumberValidationTest.java
@@ -14,47 +14,47 @@ public class CardNumberValidationTest {
 
     @Test
     public void shouldHaveCardNumberEntryCompleteWhenVisa() {
-        CardNumberValidation cardNumberValidation = new CardNumberValidation("4282730000002397", CardType.VISA, false, false);
+        CardNumberValidation cardNumberValidation = new CardNumberValidation("4282730000002397", CardType.VISA, false, false, false);
 
         assertThat(cardNumberValidation.isEntryComplete(), is(true));
     }
 
     @Test
     public void shouldHaveCardNumberEntryCompleteWhenAmex() {
-        CardNumberValidation cardNumberValidation = new CardNumberValidation("340000041582957", CardType.AMEX, false, true);
+        CardNumberValidation cardNumberValidation = new CardNumberValidation("340000041582957", CardType.AMEX, false, false, true);
 
         assertThat(cardNumberValidation.isEntryComplete(), is(true));
     }
 
     @Test
     public void shouldValidateCardNumberAsInvalid() {
-        CardNumberValidation cardNumberValidation = new CardNumberValidation("1234567812345678", CardType.UNKNOWN, false, true);
+        CardNumberValidation cardNumberValidation = new CardNumberValidation("1234567812345678", CardType.UNKNOWN, false, false, true);
 
         assertThat(cardNumberValidation.isShowError(), is(true));
     }
 
     @Test
     public void shouldShowErrorWhenMaestroPartiallyEnteredAndMaestroNotSupported() {
-        CardNumberValidation cardNumberValidation = new CardNumberValidation("6759", CardType.MAESTRO, false, false);
+        CardNumberValidation cardNumberValidation = new CardNumberValidation("6759", CardType.MAESTRO, false, false, false);
         assertThat(cardNumberValidation.isShowError(), is(true));
     }
 
     @Test
     public void shouldShowErrorWhenAmexPartiallyEnteredAndAmexNotSupported() {
-        CardNumberValidation cardNumberValidation = new CardNumberValidation("3400", CardType.AMEX, false, false);
+        CardNumberValidation cardNumberValidation = new CardNumberValidation("3400", CardType.AMEX, false, false, false);
         assertThat(cardNumberValidation.isShowError(), is(true));
     }
 
     @Test
     public void shouldHaveCardNumberErrorWhenInvalid() {
-        CardNumberValidation cardNumberValidation = new CardNumberValidation("1234567812345678", CardType.UNKNOWN, false, false);
+        CardNumberValidation cardNumberValidation = new CardNumberValidation("1234567812345678", CardType.UNKNOWN, false, false, false);
 
         assertThat(cardNumberValidation.getError(), is(R.string.error_card_number));
     }
 
     @Test
     public void shouldHaveCardErrorWhenMaestroEnteredAndMaestroNotSupported() {
-        CardNumberValidation cardNumberValidation = new CardNumberValidation("6759649826438453", CardType.MAESTRO, false, false);
+        CardNumberValidation cardNumberValidation = new CardNumberValidation("6759649826438453", CardType.MAESTRO, false, false, false);
 
         assertThat(cardNumberValidation.isShowError(), is(true));
         assertThat(cardNumberValidation.getError(), is(R.string.error_maestro_not_supported));
@@ -62,7 +62,7 @@ public class CardNumberValidationTest {
 
     @Test
     public void shouldHaveCardErrorWhenAmexEnteredAndAmexNotSupported() {
-        CardNumberValidation cardNumberValidation = new CardNumberValidation("340000432128428", CardType.AMEX, false, false);
+        CardNumberValidation cardNumberValidation = new CardNumberValidation("340000432128428", CardType.AMEX, false, false, false);
 
         assertThat(cardNumberValidation.isShowError(), is(true));
         assertThat(cardNumberValidation.getError(), is(R.string.error_amex_not_supported));
