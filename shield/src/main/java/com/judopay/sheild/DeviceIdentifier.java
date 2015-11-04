0eae71e5ec63f3cbdd343cd72b1e6c654a7ba96a
jamesgoodwin <james.goodwin@judopayment.com>
2015-11-04 15:04:33 +0000

    Judo Shield module

---
+++ shield/src/main/java/com/judopay/sheild/DeviceIdentifier.java
@@ -0,0 +1,15 @@
+package com.judopay.sheild;
+
+import android.content.Context;
+import android.provider.Settings;
+
+import java.util.Map;
+
+public class DeviceIdentifier {
+
+
+    public void addDeviceId(Map<String, String> data, Context context) {
+
+    }
+
+}
