5edfa04a2b0548df80c56e07414ba2f37254a9ca
jamesgoodwin <james.goodwin@judopayment.com>
2015-11-06 10:29:43 +0000

    Base Activity for extending all Activity classes from, providing back button

---
+++ judo-sdk/src/main/java/com/judopay/JudoActivity.java
@@ -0,0 +1,31 @@
+package com.judopay;
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+
+public class JudoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+}
