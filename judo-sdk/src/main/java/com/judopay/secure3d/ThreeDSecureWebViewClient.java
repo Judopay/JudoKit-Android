61e021264324bfd86f9d0208d73df3f895b78ee5
jamesgoodwin <james.goodwin@judopayment.com>
2015-11-10 10:11:01 +0000

    Call listener when initial page loaded Remove SSL error callback as not needed

--- judo-sdk/src/main/java/com/judopay/secure3d/ThreeDSecureWebViewClient.java
+++ judo-sdk/src/main/java/com/judopay/secure3d/ThreeDSecureWebViewClient.java
@@ -3,17 +3,18 @@ package com.judopay.secure3d;
 import android.graphics.Bitmap;
 import android.net.http.SslError;
 import android.view.View;
-import android.webkit.SslErrorHandler;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
 public class ThreeDSecureWebViewClient extends WebViewClient {
 
+    private final String acsUrl;
     private final String postbackUrl;
     private final String javaScriptNamespace;
     private final ThreeDSecureListener threeDSecureListener;
 
-    public ThreeDSecureWebViewClient(String postbackUrl, String javaScriptNamespace, ThreeDSecureListener threeDSecureListener) {
+    public ThreeDSecureWebViewClient(String acsUrl, String postbackUrl, String javaScriptNamespace, ThreeDSecureListener threeDSecureListener) {
+        this.acsUrl = acsUrl;
         this.postbackUrl = postbackUrl;
         this.javaScriptNamespace = javaScriptNamespace;
         this.threeDSecureListener = threeDSecureListener;
@@ -25,7 +26,9 @@ public class ThreeDSecureWebViewClient extends WebViewClient {
 
         view.zoomOut();
 
-        if (url.equals(postbackUrl)) {
+        if(url.equals(acsUrl)) {
+            threeDSecureListener.onAuthorizationWebPageLoaded();
+        } else if (url.equals(postbackUrl)) {
             view.loadUrl(String.format("javascript:window.%s.parseJsonFromHtml(document.getElementsByTagName('html')[0].innerHTML);", javaScriptNamespace));
         }
     }
@@ -36,12 +39,6 @@ public class ThreeDSecureWebViewClient extends WebViewClient {
         if (url.equals(postbackUrl)) {
             view.setVisibility(View.INVISIBLE);
         }
-    }
-
-    @Override
-    public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-        super.onReceivedSslError(view, handler, error);
-        // log the error or something
     }
 
     @Override
