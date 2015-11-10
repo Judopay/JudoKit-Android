642beb21b78f449b3e8ea4e99a9b328a1790875b
jamesgoodwin <james.goodwin@judopayment.com>
2015-11-10 10:09:53 +0000

    Add web page loaded callback to 3D-Secure listener

--- judo-sdk/src/main/java/com/judopay/secure3d/ThreeDSecureListener.java
+++ judo-sdk/src/main/java/com/judopay/secure3d/ThreeDSecureListener.java
@@ -4,6 +4,8 @@ import com.judopay.payment.ThreeDSecureInfo;
 
 public interface ThreeDSecureListener {
 
+    void onAuthorizationWebPageLoaded();
+
     void onAuthorizationCompleted(ThreeDSecureInfo threeDSecureInfo, String receiptId);
 
     void onAuthorizationWebPageLoadingError(int errorCode, String description, String failingUrl);
