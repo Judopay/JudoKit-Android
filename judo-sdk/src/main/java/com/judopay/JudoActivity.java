008b60b9d74773995887196d4b4de3e01b369390
jamesgoodwin <james.goodwin@judopayment.com>
2015-11-16 16:27:26 +0000

    Fix javadoc link Check in SDK configuration if rooted devices should be allowed before throwing exception

--- judo-sdk/src/main/java/com/judopay/JudoActivity.java
+++ judo-sdk/src/main/java/com/judopay/JudoActivity.java
@@ -12,9 +12,9 @@ import com.judopay.security.RootUserBlockedException;
  * Base Activity class from which all other Activities should extend from.
  * This class provides two main functions:
  * <ol>
- *     <li>Detect if the device is rooted, and throws a @see com.judopay.security.RootUserBlockedException,
- *     preventing further access since we cannot guarantee the payment transaction will be secure.</li>
- *     <li>Shows the back button in the action bar, allowing the user to navigate back easily.</li>
+ * <li>Detect if the device is rooted, and throws a {@link com.judopay.security.RootUserBlockedException},
+ * preventing further access since we cannot guarantee the payment transaction will be secure.</li>
+ * <li>Shows the back button in the action bar, allowing the user to navigate back easily.</li>
  * </ol>
  */
 public class JudoActivity extends AppCompatActivity {
@@ -23,7 +23,7 @@ public class JudoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        if(RootDetector.isRooted()) {
+        if (RootDetector.isRooted() && !JudoPay.isRootedDevicesAllowed()) {
             throw new RootUserBlockedException();
         }
 
