40d1d898dfc5e60147936e72cc0b816428e3befd
jamesgoodwin <james.goodwin@judopayment.com>
2015-11-16 13:39:28 +0000

    New class for detecting if the current user has root permission and therefore we cannot guarantee the security of a payment transaction.

---
+++ judo-sdk/src/main/java/com/judopay/security/RootDetector.java
@@ -0,0 +1,66 @@
+package com.judopay.security;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * Detects if the Android device being used has been rooted. This is done using a variety of
+ * methods, such as attempting to access system paths or run system processes that are only
+ * accessible when the device is rooted. The methods are not full-proof as if the device is rooted,
+ * anything could be changed to simulate an un-rooted device.
+ * Credit to Kevin Kowalewski for providing the root detection methods used.
+ */
+public class RootDetector {
+
+    /**
+     * Detects using several methods if the device is rooted.
+     * @return if the device is rooted
+     */
+    public static boolean isRooted() {
+        return isTestKeysPresent() || isSystemPathAccessible() || isSystemCommandExecutable();
+    }
+
+    private static boolean isTestKeysPresent() {
+        String buildTags = android.os.Build.TAGS;
+        return buildTags != null && buildTags.contains("test-keys");
+    }
+
+    private static boolean isSystemPathAccessible() {
+        String[] paths = {"/system/app/Superuser.apk",
+                "/sbin/su",
+                "/system/bin/su",
+                "/system/xbin/su",
+                "/data/local/xbin/su",
+                "/data/local/bin/su",
+                "/system/sd/xbin/su",
+                "/system/bin/failsafe/su",
+                "/data/local/su"};
+
+        for (String path : paths) {
+            if (new File(path).exists()) return true;
+        }
+        return false;
+    }
+
+    private static boolean isSystemCommandExecutable() {
+        Process process = null;
+        try {
+            process = new ProcessBuilder()
+                    .command("/system/bin/ping", "android.com")
+                    .redirectErrorStream(true)
+                    .start();
+            BufferedReader in = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            return in.readLine() != null;
+        } catch (IOException ignore) {
+        } finally {
+            if (process != null) {
+                process.destroy();
+            }
+        }
+        return false;
+    }
+
+}
+
