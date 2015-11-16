1edfe166eddd3df9396d3bc7cc6892abb63226e2
jamesgoodwin <james.goodwin@judopayment.com>
2015-11-16 13:40:20 +0000

    Add javadoc comments Detect if device is rooted and prevent access if detected

--- judo-sdk/src/main/java/com/judopay/JudoActivity.java
+++ judo-sdk/src/main/java/com/judopay/JudoActivity.java
@@ -5,11 +5,27 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 
+import com.judopay.security.RootDetector;
+import com.judopay.security.RootUserBlockedException;
+
+/**
+ * Base Activity class from which all other Activities should extend from.
+ * This class provides two main functions:
+ * <ol>
+ *     <li>Detect if the device is rooted, and throws a @see com.judopay.security.RootUserBlockedException,
+ *     preventing further access since we cannot guarantee the payment transaction will be secure.</li>
+ *     <li>Shows the back button in the action bar, allowing the user to navigate back easily.</li>
+ * </ol>
+ */
 public class JudoActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        if(RootDetector.isRooted()) {
+            throw new RootUserBlockedException();
+        }
 
         ActionBar actionBar = getSupportActionBar();
         if (actionBar != null) {
