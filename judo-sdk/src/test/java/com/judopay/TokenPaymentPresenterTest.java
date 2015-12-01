31efa2dd9fe925ecfe70d7da1e96373419863683
jamesgoodwin <james.goodwin@judopayment.com>
2015-12-01 17:07:56 +0000

    Add tests for handling the result from 3D-Secure

--- judo-sdk/src/test/java/com/judopay/TokenPaymentPresenterTest.java
+++ judo-sdk/src/test/java/com/judopay/TokenPaymentPresenterTest.java
@@ -5,6 +5,7 @@ import com.judopay.model.Card;
 import com.judopay.model.CardToken;
 import com.judopay.model.Consumer;
 import com.judopay.model.Receipt;
+import com.judopay.model.ThreeDSecureInfo;
 import com.judopay.model.TokenTransaction;
 
 import org.junit.Test;
@@ -15,6 +16,7 @@ import org.mockito.runners.MockitoJUnitRunner;
 import rx.Observable;
 
 import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
@@ -42,6 +44,9 @@ public class TokenPaymentPresenterTest {
     @Mock
     PaymentFormView paymentFormView;
 
+    @Mock
+    ThreeDSecureInfo threeDSecureInfo;
+
     Scheduler scheduler = new TestScheduler();
 
     @Test
@@ -54,4 +59,42 @@ public class TokenPaymentPresenterTest {
         verify(paymentFormView).showLoading();
         verify(apiService).tokenPayment(any(TokenTransaction.class));
     }
+
+    @Test
+    public void shouldShowWebViewWhenAuthWebPageLoaded() {
+        TokenPaymentPresenter presenter = new TokenPaymentPresenter(paymentFormView, apiService, scheduler);
+
+        presenter.onAuthorizationWebPageLoaded();
+
+        verify(paymentFormView).show3dSecureWebView();
+    }
+
+    @Test
+    public void shouldFinishWhenSuccessfulReceipt() {
+        TokenPaymentPresenter presenter = new TokenPaymentPresenter(paymentFormView, apiService, scheduler);
+        String receiptId = "123456";
+
+        when(receipt.isSuccess()).thenReturn(true);
+        when(apiService.threeDSecurePayment(receiptId, threeDSecureInfo)).thenReturn(Observable.just(receipt));
+
+        presenter.onAuthorizationCompleted(threeDSecureInfo, receiptId);
+
+        verify(paymentFormView).finish(eq(receipt));
+        verify(paymentFormView).hideLoading();
+    }
+
+    @Test
+    public void shouldShowDeclinedMessageWhenDeclinedReceipt() {
+        TokenPaymentPresenter presenter = new TokenPaymentPresenter(paymentFormView, apiService, scheduler);
+        String receiptId = "123456";
+
+        when(receipt.isSuccess()).thenReturn(false);
+        when(apiService.threeDSecurePayment(receiptId, threeDSecureInfo)).thenReturn(Observable.just(receipt));
+
+        presenter.onAuthorizationCompleted(threeDSecureInfo, "123456");
+
+        verify(paymentFormView).showDeclinedMessage(eq(receipt));
+        verify(paymentFormView).hideLoading();
+    }
+
 }
