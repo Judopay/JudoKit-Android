3b08ab140dafabe28a2c7f180d3ac5ffa8dac2fd
jamesgoodwin <james.goodwin@judopayment.com>
2015-12-07 10:56:17 +0000

    3d-Secure error callback no longer in interface

--- judo-sdk/src/main/java/com/judopay/secure3d/ThreeDSecureWebViewClient.java
+++ judo-sdk/src/main/java/com/judopay/secure3d/ThreeDSecureWebViewClient.java
@@ -47,13 +47,4 @@ class ThreeDSecureWebViewClient extends WebViewClient {
         this.resultPageListener = resultPageListener;
     }
 
-    @Override
-    public void onReceivedError(WebView view, int errorCode, String description, String failingUrl) {
-        super.onReceivedError(view, errorCode, description, failingUrl);
-
-        if (!failingUrl.startsWith(redirectUrl)) {
-            threeDSecureListener.onAuthorizationWebPageLoadingError(errorCode, description, failingUrl);
-        }
-    }
-
 }
