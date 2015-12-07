9457d02962e690266a87163dceee09c433a4d70a
jamesgoodwin <james.goodwin@judopayment.com>
2015-12-07 10:54:49 +0000

    Fix imports

--- judo-sdk/src/test/java/com/judopay/TokenPreAuthPresenterTest.java
+++ judo-sdk/src/test/java/com/judopay/TokenPreAuthPresenterTest.java
@@ -1,10 +1,9 @@
 package com.judopay;
 
+import com.google.gson.Gson;
 import com.judopay.model.Address;
 import com.judopay.model.Card;
 import com.judopay.model.CardToken;
-import com.judopay.model.Consumer;
-import com.judopay.model.PaymentTransaction;
 import com.judopay.model.Receipt;
 import com.judopay.model.TokenTransaction;
 
