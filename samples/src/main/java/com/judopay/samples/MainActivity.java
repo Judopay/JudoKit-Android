f227144a211dd4dc64bff41e74143697b9e0f12b
jamesgoodwin <james.goodwin@judopayment.com>
2016-01-21 11:22:58 +0000

    Fix for missing theme for SettingsActivity and settings preferences not saving

--- samples/src/main/java/com/judopay/samples/MainActivity.java
+++ samples/src/main/java/com/judopay/samples/MainActivity.java
@@ -72,7 +72,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
 
-        Judo.setup("823Eja2fEM6E9NAE", "382df6f458294f49f02f073e8f356f8983e2460631ea1b4c8ed4c3ee502dcbe6", Judo.Environment.SANDBOX);
+        if (savedInstanceState == null) {
+            Judo.setup("823Eja2fEM6E9NAE", "382df6f458294f49f02f073e8f356f8983e2460631ea1b4c8ed4c3ee502dcbe6", Judo.Environment.SANDBOX);
+        }
 
         paymentButton.setOnClickListener(new View.OnClickListener() {
             @Override
