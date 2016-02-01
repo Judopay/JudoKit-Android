a2e8fa9633662f6e0429aa3bec4c2b00372d17d4
jamesgoodwin <james.goodwin@judopayment.com>
2016-02-01 09:33:38 +0000

    Add UI test for pre-filling card data (e.g. card scanning)

---
+++ judo-sdk/src/androidTest/java/com/judopay/PrefillCardDataTest.java
@@ -0,0 +1,73 @@
+package com.judopay;
+
+import android.content.Intent;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import com.judopay.model.Currency;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class PrefillCardDataTest {
+
+    @Rule
+    public ActivityTestRule<PaymentActivity> activityTestRule = new ActivityTestRule<>(PaymentActivity.class, false, false);
+
+    @Before
+    public void setupJudoSdk() {
+        Judo.setup("fakeApiToken", "fakeApiSecret", Judo.Environment.SANDBOX);
+    }
+
+    @Test
+    public void shouldHaveVisaCardNumberEnteredInPaymentForm() {
+        Intent intent = new Intent();
+
+        intent.putExtra(Judo.JUDO_OPTIONS, new JudoOptions.Builder()
+                .setJudoId("100407196")
+                .setAmount("0.99")
+                .setCurrency(Currency.GBP)
+                .setCardNumber("4934123412341234")
+                .setExpiryMonth("01")
+                .setExpiryYear("20")
+                .setConsumerRef("consumerRef")
+                .build());
+
+        activityTestRule.launchActivity(intent);
+
+        onView(withId(R.id.card_number_edit_text))
+                .check(matches(withText("4934 1234 1234 1234")));
+    }
+
+    @Test
+    public void shouldHaveAmexCardNumberEnteredInPaymentForm() {
+        Intent intent = new Intent();
+
+        intent.putExtra(Judo.JUDO_OPTIONS, new JudoOptions.Builder()
+                .setJudoId("100407196")
+                .setAmount("0.99")
+                .setCurrency(Currency.GBP)
+                .setCardNumber("343412341234123")
+                .setExpiryMonth("01")
+                .setExpiryYear("20")
+                .setConsumerRef("consumerRef")
+                .build());
+
+        activityTestRule.launchActivity(intent);
+
+        onView(withId(R.id.card_number_edit_text))
+                .check(matches(withText("3434 123412 34123")));
+    }
+
+
+}
