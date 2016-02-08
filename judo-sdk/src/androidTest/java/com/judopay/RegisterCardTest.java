5594e58c578665338cf6249a6fb05ac9af9ff96a
jamesgoodwin <james.goodwin@judopayment.com>
2016-02-08 12:05:45 +0000

    Add test for register card declined dialog

---
+++ judo-sdk/src/androidTest/java/com/judopay/RegisterCardTest.java
@@ -0,0 +1,65 @@
+package com.judopay;
+
+import android.content.Intent;
+import android.support.test.espresso.intent.rule.IntentsTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class RegisterCardTest {
+
+    @Rule
+    public IntentsTestRule<RegisterCardActivity> activityTestRule = new IntentsTestRule<>(RegisterCardActivity.class, false, false);
+
+    @Before
+    public void setupJudoSdk() {
+        Judo.setup("823Eja2fEM6E9NAE", "382df6f458294f49f02f073e8f356f8983e2460631ea1b4c8ed4c3ee502dcbe6", Judo.Environment.SANDBOX);
+    }
+
+    @Test
+    public void shouldBeSuccessfulPaymentWhenValidVisaEntered() {
+        activityTestRule.launchActivity(getIntent());
+
+        onView(withId(R.id.card_number_edit_text))
+                .perform(typeText("4221690000004963"));
+
+        onView(withId(R.id.expiry_date_edit_text))
+                .perform(typeText("1220"));
+
+        onView(withId(R.id.cvv_edit_text))
+                .perform(typeText("125"));
+
+        onView(withId(R.id.payment_button))
+                .perform(click());
+
+        onView(withText(R.string.add_card_failed))
+                .check(matches(isDisplayed()));
+
+        onView(withText(R.string.please_check_details_try_again))
+                .check(matches(isDisplayed()));
+    }
+
+    public Intent getIntent() {
+        Intent intent = new Intent();
+        intent.putExtra(Judo.JUDO_OPTIONS, new JudoOptions.Builder()
+                .setJudoId("100407196")
+                .setConsumerRef("consumerRef")
+                .build());
+
+        return intent;
+    }
+}
