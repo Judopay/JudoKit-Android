cac04416a446bd81bb82c6eba2b7ae1006dbdd21
jamesgoodwin <jamesgoodwin@gmail.com>
2016-03-07 15:10:46 +0000

    Fix class not imported

--- judo-sdk/src/androidTest/java/com/judopay/ui/TokenPaymentFormTest.java
+++ judo-sdk/src/androidTest/java/com/judopay/ui/TokenPaymentFormTest.java
@@ -14,6 +14,7 @@ import com.judopay.model.CardToken;
 import com.judopay.model.CardType;
 import com.judopay.model.Currency;
 
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
