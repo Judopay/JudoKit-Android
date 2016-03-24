0887a6d481c78c678cfa6883b4cd99cf9e8356c5
jamesgoodwin <jamesgoodwin@gmail.com>
2016-03-24 13:38:32 +0000

    Replace JodaTime library when deserializing date fields in json with SimpleDateFormatter

--- judo-sdk/src/main/java/com/judopay/api/DateJsonDeserializer.java
+++ judo-sdk/src/main/java/com/judopay/api/DateJsonDeserializer.java
@@ -5,10 +5,11 @@ import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonParseException;
 
-import org.joda.time.DateTime;
-
 import java.lang.reflect.Type;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.Locale;
 
 import static com.judopay.arch.TextUtil.isEmpty;
 
@@ -19,11 +20,31 @@ class DateJsonDeserializer implements JsonDeserializer<Date> {
         String date = json.getAsString();
 
         if (!isEmpty(date)) {
-            DateTime dateTime = new DateTime(date);
-            return dateTime.toDate();
+            try {
+                return iso8601ToDate(date);
+            } catch (ParseException e) {
+                return null;
+            }
         } else {
             return null;
         }
+    }
+
+    /**
+     * Transforms a ISO 8601 string to a Date
+     *
+     * @param date the date as defined in ISO-8601 format
+     * @return the parsed date
+     * @throws ParseException
+     */
+    public Date iso8601ToDate(final String date) throws ParseException {
+        String s = date.replace("Z", "+00:00");
+        try {
+            s = s.substring(0, 22) + s.substring(23);  // to get rid of the ":"
+        } catch (IndexOutOfBoundsException e) {
+            throw new ParseException("Invalid length", 0);
+        }
+        return new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSSX", Locale.US).parse(s);
     }
 
 }
