b88af307536a794bffa6014a5a114170211f0007
jamesgoodwin <jamesgoodwin@gmail.com>
2016-03-29 15:21:44 +0100

    Connect / disconnect from GoogleApiClient depending on activity state

--- samples/src/main/java/com/judopay/samples/AndroidPayActivity.java
+++ samples/src/main/java/com/judopay/samples/AndroidPayActivity.java
@@ -3,6 +3,7 @@ package com.judopay.samples;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v7.app.ActionBar;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
@@ -20,12 +21,28 @@ public class AndroidPayActivity extends BaseActivity implements GoogleApiClient.
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_android_pay);
 
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+
         googleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(Wallet.API, new Wallet.WalletOptions.Builder()
                         .setEnvironment(WalletConstants.ENVIRONMENT_TEST)
                         .build())
                 .enableAutoManage(this, this)
                 .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        googleApiClient.connect();
+    }
+
+    public void onStop() {
+        super.onStop();
+        googleApiClient.disconnect();
     }
 
     @Override
