6de9dd2dfbdc8fba621e1b2f3d889b567212b037
jamesgoodwin <jamesgoodwin@gmail.com>
2016-03-29 10:34:48 +0100

    Include extra fields in the AndroidPay wallet request object, for identifying the the card in Judo reports

--- judo-sdk/src/main/java/com/judopay/model/Wallet.java
+++ judo-sdk/src/main/java/com/judopay/model/Wallet.java
@@ -12,14 +12,29 @@ public class Wallet {
     private final String ephemeralPublicKey;
     private final String tag;
     private final String publicKey;
+    private final String instrumentDetails;
+    private final String instrumentType;
+    private final String merchantTransactionId;
     private final Integer environment;
+    private final String version;
 
-    public Wallet(String encryptedMessage, String ephemeralPublicKey, String tag, String publicKey, Integer environment) {
+    public Wallet(String encryptedMessage,
+                  String ephemeralPublicKey,
+                  String tag,
+                  String publicKey,
+                  String instrumentDetails,
+                  String instrumentType,
+                  String merchantTransactionId,
+                  Integer environment, String version) {
         this.encryptedMessage = encryptedMessage;
         this.ephemeralPublicKey = ephemeralPublicKey;
         this.tag = tag;
         this.publicKey = publicKey;
+        this.instrumentDetails = instrumentDetails;
+        this.instrumentType = instrumentType;
+        this.merchantTransactionId = merchantTransactionId;
         this.environment = environment;
+        this.version = version;
     }
 
     public String getEncryptedMessage() {
@@ -38,8 +53,24 @@ public class Wallet {
         return publicKey;
     }
 
+    public String getInstrumentDetails() {
+        return instrumentDetails;
+    }
+
+    public String getInstrumentType() {
+        return instrumentType;
+    }
+
     public Integer getEnvironment() {
         return environment;
+    }
+
+    public String getMerchantTransactionId() {
+        return merchantTransactionId;
+    }
+
+    public String getVersion() {
+        return version;
     }
 
     public static class Builder {
@@ -48,13 +79,19 @@ public class Wallet {
         private String ephemeralPublicKey;
         private String tag;
         private String publicKey;
+        private String instrumentDetails;
+        private String instrumentType;
+        private String merchantTransactionId;
         private Integer environment;
+        private String version;
 
         /**
-         * Sets the encrypted payload JSON on the {@link AndroidPayRequest} instance, this is equivalent
-         * to calling {@link #setEncryptedMessage(String)}, {@link #setTag(String)} and {@link #setEphemeralPublicKey(String)}.
+         * Sets the encrypted payload JSON on the {@link AndroidPayRequest} instance, this is
+         * equivalent to calling {@link #setEncryptedMessage(String)}, {@link #setTag(String)}
+         * and {@link #setEphemeralPublicKey(String)}.
          *
-         * @param paymentMethodTokenJson the payment method JSON received in the Android Pay Full Wallet response
+         * @param paymentMethodTokenJson the payment method JSON received in the Android Pay
+         *                               FullWallet
          * @return The Builder for creating the {@link AndroidPayRequest} instance
          */
         public Builder setPaymentMethodToken(String paymentMethodTokenJson) {
@@ -70,7 +107,7 @@ public class Wallet {
         }
 
         /**
-         * @param encryptedMessage the encrypted message, this is required for an Android Pay transaction.
+         * @param encryptedMessage the encrypted message
          * @return The Builder for creating the {@link AndroidPayRequest} instance
          */
         public Builder setEncryptedMessage(String encryptedMessage) {
@@ -79,8 +116,8 @@ public class Wallet {
         }
 
         /**
-         * @param ephemeralPublicKey the Android Pay ephemeral public key associated with the private key to encrypt the message,
-         *                           this is required for an Android Pay transaction.
+         * @param ephemeralPublicKey the Android Pay ephemeral public key associated with the
+         *                           private key to encrypt the message.
          * @return The Builder for creating the {@link AndroidPayRequest} instance
          */
         public Builder setEphemeralPublicKey(String ephemeralPublicKey) {
@@ -89,7 +126,7 @@ public class Wallet {
         }
 
         /**
-         * @param tag the MAC of the {@link #encryptedMessage}, this is required for an Android Pay transaction.
+         * @param tag the MAC of the {@link #encryptedMessage}
          * @return The Builder for creating the {@link AndroidPayRequest} instance
          */
         public Builder setTag(String tag) {
@@ -98,11 +135,44 @@ public class Wallet {
         }
 
         /**
-         * @param publicKey the public key used for encrypting the Android Pay payment token, this is required for an Android Pay transaction.
+         * @param publicKey the public key used for encrypting the Android Pay payment token.
          * @return The Builder for creating the {@link AndroidPayRequest} instance
          */
         public Builder setPublicKey(String publicKey) {
             this.publicKey = publicKey;
+            return this;
+        }
+
+        /**
+         * @param instrumentDetails the details of the payment method used, typically this is the
+         *                          last four digits of a customer's card. This can be obtained from
+         *                          by calling getInstrumentInfo().getInstrumentDetails() on the
+         *                          FullWallet.
+         * @return The Builder for creating the {@link AndroidPayRequest} instance
+         */
+        public Builder setInstrumentDetails(String instrumentDetails) {
+            this.instrumentDetails = instrumentDetails;
+            return this;
+        }
+
+        /**
+         * @param instrumentType the type of the payment method used (i.e. VISA, MASTERCARD, AMEX).
+         *                       This can be obtained from by calling
+         *                       getInstrumentInfo().getInstrumentType() on the FullWallet
+         * @return The Builder for creating the {@link AndroidPayRequest} instance
+         */
+        public Builder setInstrumentType(String instrumentType) {
+            this.instrumentType = instrumentType;
+            return this;
+        }
+
+        /**
+         * @param merchantTransactionId the merchant ID for the transaction. This can be obtained
+         *                              from the FullWallet.getMerchantId() method.
+         * @return The Builder for creating the {@link AndroidPayRequest} instance
+         */
+        public Builder setMerchantTransactionId(String merchantTransactionId) {
+            this.merchantTransactionId = merchantTransactionId;
             return this;
         }
 
@@ -118,14 +188,26 @@ public class Wallet {
             return this;
         }
 
+        /**
+         * @param version the version of the wallet, this can be obtained from the
+         *                FullWallet.getVersionCode() method.
+         */
+        public void setVersion(String version) {
+            this.version = version;
+        }
+
         public Wallet build() {
             checkNotEmpty(encryptedMessage);
             checkNotEmpty(ephemeralPublicKey);
             checkNotEmpty(tag);
             checkNotEmpty(publicKey);
+            checkNotEmpty(instrumentDetails);
+            checkNotEmpty(instrumentType);
+            checkNotEmpty(merchantTransactionId);
             checkNotNull(environment);
+            checkNotEmpty(version);
 
-            return new Wallet(encryptedMessage, ephemeralPublicKey, tag, publicKey, environment);
+            return new Wallet(encryptedMessage, ephemeralPublicKey, tag, publicKey, instrumentDetails, instrumentType, merchantTransactionId, environment, version);
         }
     }
 
