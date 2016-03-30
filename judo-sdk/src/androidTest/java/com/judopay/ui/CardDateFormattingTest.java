016d74dd6219518a976abf41cd951ab0d326b89e
jamesgoodwin <jamesgoodwin@gmail.com>
2016-03-30 14:23:07 +0100

    Add tests for start date and expiry date text formatters

---
+++ judo-sdk/src/androidTest/java/com/judopay/ui/CardDateFormattingTest.java
@@ -0,0 +1,72 @@
+package com.judopay.ui;
+
+import android.content.Intent;
+import android.support.test.rule.ActivityTestRule;
+import android.support.test.runner.AndroidJUnit4;
+import android.test.suitebuilder.annotation.LargeTest;
+
+import com.judopay.Judo;
+import com.judopay.JudoOptions;
+import com.judopay.PaymentActivity;
+import com.judopay.R;
+import com.judopay.model.Currency;
+
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.UUID;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.typeText;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+@LargeTest
+@RunWith(AndroidJUnit4.class)
+public class CardDateFormattingTest {
+
+    @Rule
+    public ActivityTestRule<PaymentActivity> activityTestRule = new ActivityTestRule<>(PaymentActivity.class, false, false);
+
+    @Before
+    public void setupJudoSdk() {
+        Judo.setEnvironment(Judo.SANDBOX);
+    }
+
+    @Test
+    public void shouldFormatStartDate() {
+        activityTestRule.launchActivity(getIntent());
+
+        onView(withId(R.id.card_number_edit_text))
+                .perform(typeText("6759000000005462"));
+
+        onView(withId(R.id.start_date_edit_text))
+                .perform(typeText("1225"))
+                .check(matches(withText("01/16")));
+    }
+
+    @Test
+    public void shouldFormatExpiryDate() {
+        activityTestRule.launchActivity(getIntent());
+
+        onView(withId(R.id.expiry_date_edit_text))
+                .perform(typeText("1225"))
+                .check(matches(withText("12/25")));
+    }
+
+    protected Intent getIntent() {
+        Intent intent = new Intent();
+
+        intent.putExtra(Judo.JUDO_OPTIONS, new JudoOptions.Builder()
+                .setJudoId("123456")
+                .setAmount("0.99")
+                .setCurrency(Currency.GBP)
+                .setConsumerRef(UUID.randomUUID().toString())
+                .build());
+        return intent;
+    }
+
+}
