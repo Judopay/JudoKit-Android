d64134f1c7535a688fe7b207936a7d76871c0880
jamesgoodwin <jamesgoodwin@gmail.com>
2016-03-30 17:27:08 +0100

    Remove new line

--- judo-sdk/src/androidTest/java/com/judopay/ui/SecurityCodeHintTest.java
+++ judo-sdk/src/androidTest/java/com/judopay/ui/SecurityCodeHintTest.java
@@ -46,7 +46,6 @@ public class SecurityCodeHintTest {
                 .check(matches(withTextInputHint("CVV2")));
     }
 
-
     @Test
     public void shouldDisplayAmexSecurityCodeWhenAmexDetected() {
         Judo.setAmexEnabled(true);
