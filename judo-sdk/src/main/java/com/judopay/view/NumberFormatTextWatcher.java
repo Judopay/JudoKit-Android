ca623d5037c31e1973b662fdb559dda4926f340b
jamesgoodwin <jamesgoodwin@gmail.com>
2016-03-30 10:22:53 +0100

    Use NumberFormatTextWatcher as the new formatter for card number (fixes formatting issues when editing number in-place)

--- judo-sdk/src/main/java/com/judopay/view/NumberFormatTextWatcher.java
+++ judo-sdk/src/main/java/com/judopay/view/NumberFormatTextWatcher.java
@@ -2,47 +2,59 @@ package com.judopay.view;
 
 import android.text.Editable;
 import android.text.TextWatcher;
+import android.view.KeyEvent;
 import android.widget.EditText;
 
 public class NumberFormatTextWatcher implements TextWatcher {
 
     private final EditText editText;
-    private static final String FORMAT = "0000 0000 0000 0000";
     private boolean deleting;
     private int start;
 
-    public NumberFormatTextWatcher(EditText editText) {
+    private String format;
+
+    public NumberFormatTextWatcher(EditText editText, String format) {
         this.editText = editText;
+        this.format = format;
     }
 
     @Override
     public void beforeTextChanged(CharSequence s, int start, int count, int after) {
-        System.out.print("" + start + count + after);
     }
 
     @Override
     public void onTextChanged(CharSequence s, int start, int before, int count) {
-        System.out.print("" + start + before + count);
         deleting = before == 1;
         this.start = start;
     }
 
     @Override
-    public void afterTextChanged(Editable s) {
-       editText.removeTextChangedListener(this);
+    public void afterTextChanged(Editable string) {
+        editText.removeTextChangedListener(this);
+        format(string);
+        editText.addTextChangedListener(this);
+    }
 
+    public void format(Editable s) {
         for (int i = s.length(); i > 0; i--) {
-            if (s.charAt(i - 1) == ' ' || ((deleting && i == start) && FORMAT.charAt(i) == ' ')) {
+            if (s.charAt(i - 1) == ' ' || ((deleting && i == start) && format.charAt(i) == ' ')) {
                 s.delete(i - 1, i);
             }
         }
 
         for (int i = 0; i < s.length(); i++) {
-            if (i < FORMAT.length() && FORMAT.charAt(i) == ' ') {
+            if (i < format.length() && format.charAt(i) == ' ') {
                 s.insert(i, " ");
             }
         }
-
-        editText.addTextChangedListener(this);
     }
+
+    public void setFormat(String format) {
+        if (!this.format.equals(format)) {
+            this.format = format;
+            // trigger a key event to reformat the text
+            editText.dispatchKeyEvent(new KeyEvent(KeyEvent.ACTION_DOWN, KeyEvent.KEYCODE_SPACE));
+        }
+    }
+
 }
