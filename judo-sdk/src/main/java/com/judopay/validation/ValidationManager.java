1d51a380b0e1b5bfee106b01d1adf15d17b9cef9
jamesgoodwin <jamesgoodwin@gmail.com>
2016-04-21 16:21:03 +0100

    Add ability to add / remove validators at runtime

--- judo-sdk/src/main/java/com/judopay/validation/ValidationManager.java
+++ judo-sdk/src/main/java/com/judopay/validation/ValidationManager.java
@@ -4,11 +4,13 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+import rx.Subscription;
 import rx.functions.Action1;
 
 public class ValidationManager {
 
     private final Map<Validator, Boolean> validationResults;
+    private final Map<Validator, Subscription> subscriptions;
     private final OnChangeListener onValidationChangeListener;
 
     public interface OnChangeListener {
@@ -18,16 +20,39 @@ public class ValidationManager {
     public ValidationManager(List<Validator> validators, OnChangeListener onValidationChangeListener) {
         this.onValidationChangeListener = onValidationChangeListener;
         this.validationResults = new LinkedHashMap<>();
+        this.subscriptions = new LinkedHashMap<>();
 
         for (final Validator validator : validators) {
+            addValidator(validator);
+        }
+    }
+
+    public void addValidator(final Validator validator) {
+        if(!validationResults.containsKey(validator)) {
             validationResults.put(validator, false);
-            validator.onValidate().subscribe(new Action1<Validation>() {
+
+            Subscription subscription = validator.onValidate().subscribe(new Action1<Validation>() {
                 @Override
                 public void call(Validation validation) {
+                    if(validationResults.containsKey(validator)) {
+                        validationResults.remove(validator);
+                    }
                     validationResults.put(validator, validation.isValid());
                     notifyListener();
                 }
             });
+
+            subscriptions.put(validator, subscription);
+        }
+    }
+
+    public void removeValidator(final Validator validator) {
+        if(subscriptions.containsKey(validator)) {
+            Subscription subscription = subscriptions.get(validator);
+            subscription.unsubscribe();
+
+            subscriptions.remove(validator);
+            validationResults.remove(validator);
         }
     }
 
