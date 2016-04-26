db4078877f6b1854d2d257ad2955a004bfac09da
jamesgoodwin <jamesgoodwin@gmail.com>
2016-04-26 09:56:48 +0100

    Fix issue where the country spinner can't be selected during test. Move AVS tests into separate test class

--- judo-sdk/src/androidTest/java/com/judopay/PaymentFormTest.java
+++ judo-sdk/src/androidTest/java/com/judopay/PaymentFormTest.java
@@ -1,16 +1,10 @@
 package com.judopay;
 
 import android.content.Intent;
-import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 import android.test.suitebuilder.annotation.LargeTest;
 
-import com.judopay.Judo;
-import com.judopay.JudoOptions;
-import com.judopay.PaymentActivity;
-import com.judopay.R;
-import com.judopay.model.Country;
 import com.judopay.model.Currency;
 
 import org.junit.Rule;
@@ -26,8 +20,6 @@ import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withHint;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import static com.judopay.util.ViewMatchers.isNotDisplayed;
 
 @LargeTest
 @RunWith(AndroidJUnit4.class)
@@ -60,97 +52,6 @@ public class PaymentFormTest {
         onView(withId(R.id.security_code_edit_text))
                 .perform(click())
                 .check(matches(withHint("000")));
-    }
-
-    @Test
-    public void shouldDisablePostcodeFieldWhenOtherCountrySelected() {
-        Judo.setAvsEnabled(true);
-
-        activityTestRule.launchActivity(getIntent());
-
-        onView(withId(R.id.card_number_edit_text))
-                .perform(typeText("4976000000003436"));
-
-        onView(withId(R.id.expiry_date_edit_text))
-                .perform(typeText("1220"));
-
-        onView(withId(R.id.security_code_edit_text))
-                .perform(typeText("452"));
-    }
-
-    @Test
-    public void shouldShowPaymentButtonWhenAvsEnabledAndOtherCountrySelected() {
-        Judo.setAvsEnabled(true);
-
-        activityTestRule.launchActivity(getIntent());
-
-        onView(withId(R.id.card_number_edit_text))
-                .perform(typeText("4976000000003436"));
-
-        onView(withId(R.id.expiry_date_edit_text))
-                .perform(typeText("1220"));
-
-        onView(withId(R.id.security_code_edit_text))
-                .perform(typeText("452"));
-
-        onView(withId(R.id.country_spinner))
-                .perform(click());
-
-        onView(withText(Country.OTHER))
-                .perform(click());
-
-        onView(withId(R.id.payment_button))
-                .check(matches(isDisplayed()));
-    }
-
-    @Test
-    public void shouldNotShowPaymentButtonWhenAvsEnabledAndCountrySelected() {
-        Judo.setAvsEnabled(true);
-
-        activityTestRule.launchActivity(getIntent());
-
-        onView(withId(R.id.card_number_edit_text))
-                .perform(typeText("4976000000003436"));
-
-        onView(withId(R.id.expiry_date_edit_text))
-                .perform(typeText("1220"));
-
-        onView(withId(R.id.security_code_edit_text))
-                .perform(typeText("452"));
-
-        onView(withId(R.id.country_spinner))
-                .perform(click());
-
-        onView(withText(Country.UNITED_KINGDOM))
-                .perform(click());
-
-        onView(withId(R.id.payment_button))
-                .check(matches(isNotDisplayed()));
-    }
-
-    @Test
-    public void shouldNotAllowPaymentWhenAvsEnabledAndPostcodeNotEntered() {
-        Judo.setAvsEnabled(true);
-
-        activityTestRule.launchActivity(getIntent());
-
-        onView(withId(R.id.card_number_edit_text))
-                .perform(typeText("4976000000003436"));
-
-        onView(withId(R.id.expiry_date_edit_text))
-                .perform(typeText("1220"));
-
-        onView(withId(R.id.security_code_edit_text))
-                .perform(typeText("452"));
-
-        onView(withId(R.id.country_spinner))
-                .perform(click());
-
-        onView(withText(Country.UNITED_KINGDOM))
-                .perform(click());
-
-        onView(withId(R.id.payment_button))
-                .check(matches(isNotDisplayed()));
     }
 
     @Test
