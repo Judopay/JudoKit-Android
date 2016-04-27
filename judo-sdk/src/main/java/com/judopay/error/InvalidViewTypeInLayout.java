3398b35165f9aa326073b5f941968f83b101df80
jamesgoodwin <jamesgoodwin@gmail.com>
2016-04-27 10:51:33 +0100

    Provide better error message if an invalid view is detected in a custom layout

--- judo-sdk/src/main/java/com/judopay/error/InvalidViewTypeInLayout.java
+++ judo-sdk/src/main/java/com/judopay/error/InvalidViewTypeInLayout.java
@@ -1,5 +1,13 @@
 package com.judopay.error;
 
+import android.view.View;
+
+import java.util.Locale;
+
 public class InvalidViewTypeInLayout extends Error {
+
+    public InvalidViewTypeInLayout(Class<? extends View> viewClass) {
+        super(String.format(Locale.US, "View class %s is not allowed inside custom layout", viewClass.getName()));
+    }
 
 }
