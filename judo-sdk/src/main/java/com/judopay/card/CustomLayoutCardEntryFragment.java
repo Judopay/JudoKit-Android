e19972787430a9eedbee5d0fd55c8b15ed83c1a5
jamesgoodwin <jamesgoodwin@gmail.com>
2016-04-27 13:23:14 +0100

    Set EditText input rules in Custom layout fragment (previously in layout file)

--- judo-sdk/src/main/java/com/judopay/card/CustomLayoutCardEntryFragment.java
+++ judo-sdk/src/main/java/com/judopay/card/CustomLayoutCardEntryFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.design.widget.TextInputLayout;
 import android.text.InputFilter;
 import android.text.InputType;
+import android.text.method.DigitsKeyListener;
 import android.util.Pair;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -12,7 +13,6 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
 
-import com.judopay.model.CustomLayout;
 import com.judopay.Judo;
 import com.judopay.JudoOptions;
 import com.judopay.R;
@@ -20,6 +20,7 @@ import com.judopay.model.Address;
 import com.judopay.model.Card;
 import com.judopay.model.CardNetwork;
 import com.judopay.model.Country;
+import com.judopay.model.CustomLayout;
 import com.judopay.validation.CardNumberValidator;
 import com.judopay.validation.CountryAndPostcodeValidator;
 import com.judopay.validation.ExpiryDateValidator;
@@ -27,10 +28,10 @@ import com.judopay.validation.IssueNumberValidator;
 import com.judopay.validation.SecurityCodeValidator;
 import com.judopay.validation.StartDateValidator;
 import com.judopay.validation.Validation;
+import com.judopay.validation.ValidationAutoAdvanceManager;
 import com.judopay.validation.ValidationManager;
 import com.judopay.validation.Validator;
 import com.judopay.view.CountrySpinnerAdapter;
-import com.judopay.validation.ValidationAutoAdvanceManager;
 import com.judopay.view.HintFocusListener;
 import com.judopay.view.NumberFormatTextWatcher;
 import com.judopay.view.SimpleTextWatcher;
@@ -59,7 +60,6 @@ public final class CustomLayoutCardEntryFragment extends AbstractCardEntryFragme
     private Spinner countrySpinner;
 
     private ValidationManager validationManager;
-    private ValidationManager avsValidationManager;
 
     private StartDateValidator startDateValidator;
     private IssueNumberValidator issueNumberValidator;
@@ -127,44 +127,70 @@ public final class CustomLayoutCardEntryFragment extends AbstractCardEntryFragme
     }
 
     private void initializeInputTexts() {
-        EditText cardNumberTextInputEditText = cardNumberTextInput.getEditText();
+        EditText cardNumberEditText = cardNumberTextInput.getEditText();
 
-        if (cardNumberTextInputEditText != null) {
-            cardNumberTextInputEditText.setOnFocusChangeListener(new HintFocusListener(cardNumberTextInputEditText, getResources().getString(R.string.card_number_format)));
-            NumberFormatTextWatcher numberFormatTextWatcher = new NumberFormatTextWatcher(cardNumberTextInputEditText, getResources().getString(R.string.card_number_format));
-            cardNumberTextInputEditText.addTextChangedListener(numberFormatTextWatcher);
+        if (cardNumberEditText != null) {
+            initNumericEditText(cardNumberEditText, 19, "0123456789 ");
+
+            cardNumberEditText.setOnFocusChangeListener(new HintFocusListener(cardNumberEditText, getResources().getString(R.string.card_number_format)));
+            NumberFormatTextWatcher numberFormatTextWatcher = new NumberFormatTextWatcher(cardNumberEditText, getResources().getString(R.string.card_number_format));
+
+            cardNumberEditText.addTextChangedListener(numberFormatTextWatcher);
         }
 
         EditText expiryDateEditText = expiryDateTextInput.getEditText();
+
         if (expiryDateEditText != null) {
             initializeDateEditText(expiryDateEditText);
         }
 
         startDateTextInput.setVisibility(View.GONE);
         EditText startDateEditText = startDateTextInput.getEditText();
+
         if (startDateEditText != null) {
             initializeDateEditText(startDateEditText);
         }
 
         issueNumberTextInput.setVisibility(View.GONE);
+        EditText issueNumberEditText = issueNumberTextInput.getEditText();
+
+        if (issueNumberEditText != null) {
+            initNumericEditText(issueNumberEditText, 2, "0123456789");
+        }
 
         EditText securityCodeEditText = securityCodeTextInput.getEditText();
+
         if (securityCodeEditText != null) {
+            initNumericEditText(securityCodeEditText, 3, "0123456789");
             securityCodeHintFocusChangeListener = new HintFocusListener(securityCodeEditText, "000");
             securityCodeEditText.setOnFocusChangeListener(securityCodeHintFocusChangeListener);
         }
 
         countrySpinner.setVisibility(View.GONE);
         postcodeTextInput.setVisibility(View.GONE);
+
+        EditText postcodeEditText = postcodeTextInput.getEditText();
+
+        if (postcodeEditText != null) {
+            postcodeEditText.setFilters(new InputFilter[]{new InputFilter.AllCaps()});
+        }
     }
 
     private void initializeDateEditText(EditText editText) {
+        initNumericEditText(editText, 5, "0123456789/");
+
         HintFocusListener hintFocusListener = new HintFocusListener(editText, getString(R.string.date_hint));
         editText.setOnFocusChangeListener(hintFocusListener);
 
         String dateFormat = getResources().getString(R.string.date_format);
         NumberFormatTextWatcher numberFormatTextWatcher = new NumberFormatTextWatcher(editText, dateFormat, true);
         editText.addTextChangedListener(numberFormatTextWatcher);
+    }
+
+    private void initNumericEditText(EditText editText, int maxLength, String acceptedChars) {
+        editText.setRawInputType(InputType.TYPE_CLASS_NUMBER);
+        editText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(maxLength)});
+        editText.setKeyListener(DigitsKeyListener.getInstance(acceptedChars));
     }
 
     private void initializeValidators() {
@@ -193,21 +219,11 @@ public final class CustomLayoutCardEntryFragment extends AbstractCardEntryFragme
                         validationManager.addValidator(issueNumberValidator);
                         validationManager.addValidator(startDateValidator);
 
-                        if (isAvsEnabled()) {
-                            avsValidationManager.addValidator(startDateValidator);
-                            avsValidationManager.addValidator(issueNumberValidator);
-                        }
-
                         startDateTextInput.setVisibility(View.VISIBLE);
                         issueNumberTextInput.setVisibility(View.VISIBLE);
                     } else {
                         validationManager.removeValidator(startDateValidator);
                         validationManager.removeValidator(issueNumberValidator);
-
-                        if (isAvsEnabled()) {
-                            avsValidationManager.removeValidator(startDateValidator);
-                            avsValidationManager.removeValidator(issueNumberValidator);
-                        }
 
                         startDateTextInput.setVisibility(View.GONE);
                         issueNumberTextInput.setVisibility(View.GONE);
@@ -237,25 +253,28 @@ public final class CustomLayoutCardEntryFragment extends AbstractCardEntryFragme
         validationManager = new ValidationManager(validators, this);
 
         if (isAvsEnabled()) {
-            ArrayList<Validator> avsValidators = new ArrayList<>();
-            avsValidators.add(cardNumberValidator);
-            avsValidators.add(expiryDateValidator);
-            avsValidators.add(securityCodeValidator);
-
-            avsValidationManager = new ValidationManager(avsValidators, new ValidationManager.OnChangeListener() {
-                @Override
-                public void onValidate(boolean valid) {
-                    countrySpinner.setVisibility(valid ? View.VISIBLE : View.GONE);
-                    postcodeTextInput.setVisibility(valid ? View.VISIBLE : View.GONE);
-                }
-            });
-            initializeAvsValidators(validatorViews);
+            initializeAvsValidators(validatorViews, cardNumberValidator, expiryDateValidator);
         }
 
         new ValidationAutoAdvanceManager(validationManager, validatorViews);
     }
 
-    private void initializeAvsValidators(List<Pair<Validator, View>> validatorViews) {
+    private void initializeAvsValidators(List<Pair<Validator, View>> validatorViews,
+                                         CardNumberValidator cardNumberValidator,
+                                         ExpiryDateValidator expiryDateValidator) {
+        ArrayList<Validator> avsValidators = new ArrayList<>();
+        avsValidators.add(cardNumberValidator);
+        avsValidators.add(expiryDateValidator);
+        avsValidators.add(securityCodeValidator);
+
+        new ValidationManager(avsValidators, new ValidationManager.OnChangeListener() {
+            @Override
+            public void onValidate(boolean valid) {
+                countrySpinner.setVisibility(valid ? View.VISIBLE : View.GONE);
+                postcodeTextInput.setVisibility(valid ? View.VISIBLE : View.GONE);
+            }
+        });
+
         CountryAndPostcodeValidator countryAndPostcodeValidator = new CountryAndPostcodeValidator(countrySpinner, postcodeTextInput.getEditText());
         ConnectableObservable<Validation> observable = countryAndPostcodeValidator.onValidate();
 
@@ -269,12 +288,7 @@ public final class CustomLayoutCardEntryFragment extends AbstractCardEntryFragme
                 } else {
                     postcodeTextInput.setError("");
                 }
-            }
-        });
 
-        observable.subscribe(new Action1<Validation>() {
-            @Override
-            public void call(Validation validation) {
                 String country = (String) countrySpinner.getSelectedItem();
                 postcodeTextInput.setHint(getResources().getString(Country.postcodeName(country)));
                 boolean postcodeNumeric = Country.UNITED_STATES.equals(country);
