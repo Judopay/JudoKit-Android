f083805a74b1a5ace658059583dede32be9efd5b
jamesgoodwin <jamesgoodwin@gmail.com>
2016-06-01 15:20:39 +0100

    Prevent multiple emulators from launching in CI environment

--- start-emulator.sh
+++ start-emulator.sh
@@ -5,20 +5,17 @@ if [[ $# -eq 0 ]] ; then
     exit 1
 fi
 
-if [[ $(adb devices | grep 'emulator') = *emulator* ]]; then
-    echo "Android device already running, rebooting"
-    adb emu kill
+if ! [[ $(adb devices | grep 'emulator') = *emulator* ]]; then
+	emulator -avd $1 &
+
+	OUT=`adb shell getprop init.svc.bootanim`
+	RES="stopped"
+
+	while [[ ${OUT:0:7}  != 'stopped' ]]; do
+    		OUT=`adb shell getprop init.svc.bootanim`
+    		echo 'Waiting for emulator to fully boot...'
+    		sleep 5
+	done
+
+	adb shell input keyevent 82
 fi
-
-emulator -avd $1 &
-
-OUT=`adb shell getprop init.svc.bootanim`
-RES="stopped"
-
-while [[ ${OUT:0:7}  != 'stopped' ]]; do
-    OUT=`adb shell getprop init.svc.bootanim`
-    echo 'Waiting for emulator to fully boot...'
-    sleep 5
-done
-
-adb shell input keyevent 82
