c05eea6522cb24f2e9320b58c86c8c0dd669bfff
jamesgoodwin <jamesgoodwin@gmail.com>
2016-06-07 17:23:16 +0100

    Revert changes

--- samples/src/main/java/com/judopay/samples/MainActivity.java
+++ samples/src/main/java/com/judopay/samples/MainActivity.java
@@ -36,9 +36,9 @@ public class MainActivity extends BaseActivity {
 
     private static final String AMOUNT = "0.99";
 
-    private static final String JUDO_ID = "100407-196";
-    private static final String API_TOKEN = "823Eja2fEM6E9NAE";
-    private static final String API_SECRET = "382df6f458294f49f02f073e8f356f8983e2460631ea1b4c8ed4c3ee502dcbe6";
+    private static final String JUDO_ID = "<JUDO_ID>";
+    private static final String API_TOKEN = "<API_TOKEN>";
+    private static final String API_SECRET = "<API_SECRET>";
 
     private static final String CONSUMER_REF = "AndroidSdkSampleConsumerRef";
 
