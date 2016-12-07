7398f3b4b5811c8ad424525a008a0c2092f342d7
James Goodwin <jamesgoodwin@gmail.com>
2016-12-07 14:02:33 +0000

    Fix errors after merge

--- judo-sdk/src/androidTest/java/com/judopay/PrefillCardDataTest.java
+++ judo-sdk/src/androidTest/java/com/judopay/PrefillCardDataTest.java
@@ -9,6 +9,8 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.UUID;
+
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
