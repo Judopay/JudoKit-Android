7e5740305365b30453eb63ba7c52c7434625ebfe
Zeno Foltin <zeno.foltin@gmail.com>
2019-01-25 17:02:06 +0000

    Google Pay
    - Google pay implementation
    - Bump version to 7.0.0

---
+++ judo-sdk/src/main/java/com/judopay/model/GooglePayRequest.java
@@ -0,0 +1,108 @@
+package com.judopay.model;
+
+import java.util.Map;
+
+import static com.judopay.arch.Preconditions.checkNotEmpty;
+
+/**
+ * A request for performing a payment or pre-auth with the Judopay API using Google Pay.
+ * This contains information from the Google Play Services and the common request fields used when
+ * performing a transaction.
+ */
+@SuppressWarnings({"unused"})
+public final class GooglePayRequest extends BasePaymentRequest {
+    private final GooglePayWallet googlePayWallet;
+
+    private GooglePayRequest(GooglePayWallet googlePayWallet) {
+        this.googlePayWallet = googlePayWallet;
+    }
+
+    public GooglePayWallet getWallet() {
+        return googlePayWallet;
+    }
+
+    public static class Builder {
+        private String amount;
+        private String currency;
+        private String judoId;
+        private String consumerReference;
+        private Map<String, String> metaData;
+        private GooglePayWallet googlePayWallet;
+
+        /**
+         * @param amount the amount for the transaction
+         * @return The Builder for creating the {@link GooglePayRequest} instance
+         */
+        public Builder setAmount(String amount) {
+            this.amount = amount;
+            return this;
+        }
+
+        /**
+         * @param currency a currency code string as defined in the list of supported currencies in {@link Currency}
+         * @return The Builder for creating the {@link GooglePayRequest} instance
+         */
+        public Builder setCurrency(String currency) {
+            this.currency = currency;
+            return this;
+        }
+
+        /**
+         * @param judoId the judo ID of the Judo account
+         * @return The Builder for creating the {@link GooglePayRequest} instance
+         */
+        public Builder setJudoId(String judoId) {
+            this.judoId = judoId;
+            return this;
+        }
+
+        /**
+         * @param consumerReference your reference to identify the transaction
+         * @return The Builder for creating the {@link GooglePayRequest} instance
+         */
+        public Builder setConsumerReference(String consumerReference) {
+            this.consumerReference = consumerReference;
+            return this;
+        }
+
+        /**
+         * @param metaData data to be passed with the transaction that will be returned in the response
+         * @return The Builder for creating the {@link GooglePayRequest} instance
+         */
+        public Builder setMetaData(Map<String, String> metaData) {
+            this.metaData = metaData;
+            return this;
+        }
+
+        /**
+         * @param googlePayWallet the GooglePayWallet information from Google Pay
+         * @return The Builder for creating the {@link GooglePayRequest} instance
+         */
+        public Builder setGooglePayWallet(GooglePayWallet googlePayWallet) {
+            this.googlePayWallet = googlePayWallet;
+            return this;
+        }
+
+        /**
+         * Creates the {@link GooglePayRequest} instance with the fields from the {@link Builder}
+         *
+         * @return the built {@link GooglePayRequest} instance
+         */
+        public GooglePayRequest build() {
+            checkNotEmpty(amount);
+            checkNotEmpty(currency);
+            checkNotEmpty(judoId);
+            checkNotEmpty(consumerReference);
+            checkNotNull(googlePayWallet);
+
+            GooglePayRequest request = new GooglePayRequest(googlePayWallet);
+            request.amount = amount;
+            request.currency = currency;
+            request.judoId = judoId;
+            request.yourConsumerReference = consumerReference;
+            request.yourPaymentMetaData = metaData;
+
+            return request;
+        }
+    }
+}
