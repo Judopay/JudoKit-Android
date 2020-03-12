f4e6df798665177d40600914a2b274b20fb1425d
marcghelas <marc.ghelas@endava.com>
2020-03-12 15:33:17 +0200

    -change bold displaying logic

--- judo-sdk/src/main/java/com/judopay/ui/paymentmethods/adapter/viewholder/SavedCardsItemViewHolder.kt
+++ judo-sdk/src/main/java/com/judopay/ui/paymentmethods/adapter/viewholder/SavedCardsItemViewHolder.kt
@@ -1,11 +1,9 @@
 package com.judopay.ui.paymentmethods.adapter.viewholder
 
-import android.graphics.Typeface
-import android.text.Spannable
-import android.text.SpannableStringBuilder
-import android.text.style.StyleSpan
+import android.text.Spanned
 import android.view.View
 import androidx.core.content.ContextCompat
+import androidx.core.text.HtmlCompat
 import androidx.recyclerview.widget.RecyclerView
 import com.judopay.R
 import com.judopay.model.displayName
@@ -19,66 +17,51 @@ import kotlinx.android.synthetic.main.saved_card_item.view.title
 import java.text.SimpleDateFormat
 import java.util.*
 
-class SavedCardsItemViewHolder(view: View) : RecyclerView.ViewHolder(view), BindableRecyclerViewHolder<PaymentMethodSavedCardItem, PaymentMethodItemAction> {
-    override fun bind(model: PaymentMethodSavedCardItem, listener: PaymentMethodsAdapterListener?) = with(itemView) {
+class SavedCardsItemViewHolder(view: View) : RecyclerView.ViewHolder(view),
+    BindableRecyclerViewHolder<PaymentMethodSavedCardItem, PaymentMethodItemAction> {
+    override fun bind(model: PaymentMethodSavedCardItem, listener: PaymentMethodsAdapterListener?) =
+        with(itemView) {
+            subTitle.text = getSubtitleByExpiryDate(model)
+            title.text = model.title
+            val checkMark = if (model.isSelected) R.drawable.ic_radio_on else R.drawable.ic_radio_off
+            radioIconImageView.setImageResource(checkMark)
 
-        subTitle.text = createBoldSubtitle(model)
-        title.text = model.title
-        val checkMark = if (model.isSelected) R.drawable.ic_radio_on else R.drawable.ic_radio_off
-        radioIconImageView.setImageResource(checkMark)
+            setOnClickListener { listener?.invoke(PaymentMethodItemAction.PICK_CARD, model) }
+        }
 
-        setOnClickListener { listener?.invoke(PaymentMethodItemAction.PICK_CARD, model) }
-    }
-
-    private fun createBoldSubtitle(model: PaymentMethodSavedCardItem): SpannableStringBuilder {
+    private fun getSubtitleByExpiryDate(model: PaymentMethodSavedCardItem): Spanned {
         val today = Date()
         val expiryDate = SimpleDateFormat("MM/yy", Locale.UK).parse(model.expireDate) ?: today
         val twoMonths = Calendar.getInstance().apply { add(Calendar.MONTH, 2) }.time
-        val boldString = SpannableStringBuilder()
         with(itemView) {
-            val cardSubtitle = SpannableStringBuilder(
-                context.getString(
-                    R.string.card_subtitle,
-                    model.network.displayName
+            return when {
+                expiryDate.before(today) -> {
+                    subTitle.setTextColor(ContextCompat.getColor(context, R.color.tomato_red))
+                    HtmlCompat.fromHtml(
+                        resources.getString(
+                            R.string.card_subtitle_expired,
+                            model.network.displayName,
+                            model.ending
+                        ), HtmlCompat.FROM_HTML_MODE_LEGACY
+                    )
+                }
+                expiryDate.after(today) && expiryDate.before(twoMonths) -> {
+                    HtmlCompat.fromHtml(
+                        resources.getString(
+                            R.string.card_subtitle_will_expire_soon,
+                            model.network.displayName,
+                            model.ending
+                        ), HtmlCompat.FROM_HTML_MODE_LEGACY
+                    )
+                }
+                else -> HtmlCompat.fromHtml(
+                    resources.getString(
+                        R.string.card_subtitle,
+                        model.network.displayName,
+                        model.ending
+                    ), HtmlCompat.FROM_HTML_MODE_LEGACY
                 )
-            )
-        when {
-            expiryDate.before(today) -> {
-                subTitle.setTextColor(ContextCompat.getColor(context, R.color.tomato_red))
-                boldString.apply {
-                    append("${model.ending} ${resources.getString(R.string.is_expired)}")
-                    val expiredIndex =
-                        if (boldString.indexOf(resources.getString(R.string.expired), ignoreCase = true) == -1) {
-                            0
-                        } else {
-                            boldString.indexOf(resources.getString(R.string.expired), ignoreCase = true)
-                        }
-                    setSpan(StyleSpan(Typeface.BOLD), 0, boldString.length - resources.getString(R.string.is_expired).length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
-                    setSpan(StyleSpan(Typeface.BOLD), expiredIndex, boldString.length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
-                }
             }
-            expiryDate.after(today) && expiryDate.before(twoMonths) -> {
-                boldString.apply {
-                    append("${model.ending} ${resources.getString(R.string.will_expire_soon)}")
-                    val expireIndex =
-                        if (boldString.indexOf(resources.getString(R.string.expire), ignoreCase = true) == -1) {
-                            0
-                        } else {
-                            boldString.indexOf(resources.getString(R.string.expire), ignoreCase = true)
-                        }
-                    setSpan(StyleSpan(Typeface.BOLD), 0, boldString.length - resources.getString(R.string.will_expire_soon).length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
-                    setSpan(StyleSpan(Typeface.BOLD), expireIndex, boldString.length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
-                }
-            }
-            else -> boldString.apply {
-                append(model.ending)
-                setSpan(StyleSpan(Typeface.BOLD), 0, boldString.length, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
-            }
-        }
-
-        cardSubtitle.append(" ")
-        cardSubtitle.append(boldString)
-            return cardSubtitle
         }
     }
 }
