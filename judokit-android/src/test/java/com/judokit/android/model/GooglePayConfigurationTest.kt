81a0e61c7fb49e519a436778a9b4fee21ce26a04
marcghelas <56251230+marcghelas@users.noreply.github.com>
2020-05-19 11:13:13 +0300

    -GooglePayConfigurationBuilder tests (#25)

--- judokit-android/src/test/java/com/judokit/android/model/GooglePayConfigurationTest.kt
+++ judokit-android/src/test/java/com/judokit/android/model/GooglePayConfigurationTest.kt
@@ -1,7 +1,13 @@
 package com.judokit.android.model
 
+import com.judokit.android.model.googlepay.GooglePayAddressFormat
+import com.judokit.android.model.googlepay.GooglePayBillingAddressParameters
+import com.judokit.android.model.googlepay.GooglePayCheckoutOption
 import com.judokit.android.model.googlepay.GooglePayEnvironment
+import com.judokit.android.model.googlepay.GooglePayPriceStatus
 import com.judokit.android.model.googlepay.GooglePayShippingAddressParameters
+import org.junit.jupiter.api.Assertions.assertDoesNotThrow
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Test
@@ -10,7 +16,7 @@ import org.junit.jupiter.api.assertThrows
 @DisplayName("Testing the GooglePay configuration object builder logic")
 internal class GooglePayConfigurationTest {
 
-    lateinit var googlePayConfigurationBuilder: GooglePayConfiguration.Builder
+    private lateinit var googlePayConfigurationBuilder: GooglePayConfiguration.Builder
 
     @BeforeEach
     fun setUp() {
@@ -22,6 +28,44 @@ internal class GooglePayConfigurationTest {
     fun testThatBuildThrowsWhenNoEnvIsSpecified() {
         assertThrows<IllegalArgumentException> {
             googlePayConfigurationBuilder.build()
+        }
+    }
+
+    @Test
+    @DisplayName("Given transactionCountryCode is null, then build() should throw a IllegalArgumentException")
+    fun testThatBuildThrowsWhenTransactionCountryCodeNull() {
+        assertThrows<IllegalArgumentException> {
+            googlePayConfigurationBuilder
+                .setTransactionCountryCode(null)
+                .build()
+        }
+    }
+
+    @Test
+    @DisplayName("Given all required fields are specified, then build() should return GooglePayConfigurationObject")
+    fun returnGooglePayConfigurationObjectWhenAllRequiredFieldsSpecified() {
+        val billingAddressParameters =
+            GooglePayBillingAddressParameters(GooglePayAddressFormat.MIN, false)
+        val shippingAddressParams = GooglePayShippingAddressParameters(
+            allowedCountryCodes = arrayOf("US"),
+            phoneNumberRequired = true
+        )
+
+        assertDoesNotThrow {
+            googlePayConfigurationBuilder
+                .setMerchantName("Name")
+                .setTransactionId("id")
+                .setTotalPriceStatus(GooglePayPriceStatus.FINAL)
+                .setCheckoutOption(GooglePayCheckoutOption.DEFAULT)
+                .setIsEmailRequired(false)
+                .setIsBillingAddressRequired(false)
+                .setBillingAddressParameters(billingAddressParameters)
+                .setShippingAddressParameters(shippingAddressParams)
+                .setIsShippingAddressRequired(false)
+                .setTotalPriceLabel("label")
+                .setEnvironment(GooglePayEnvironment.TEST)
+                .setTransactionCountryCode("US")
+                .build()
         }
     }
 
@@ -40,5 +84,17 @@ internal class GooglePayConfigurationTest {
                 .setShippingAddressParameters(shippingAddressParams)
                 .build()
         }
+    }
+
+    @Test
+    @DisplayName("Given totalPriceStatus is null, then use FINAL default value")
+    fun useFinalDefaultValueOnTotalPriceStatusNull() {
+        val actualTotalPriceStatus = googlePayConfigurationBuilder
+            .setEnvironment(GooglePayEnvironment.TEST)
+            .setTransactionCountryCode("US")
+            .build().totalPriceStatus
+        val expectedTotalPriceStatus = GooglePayPriceStatus.FINAL
+
+        assertEquals(expectedTotalPriceStatus, actualTotalPriceStatus)
     }
 }
