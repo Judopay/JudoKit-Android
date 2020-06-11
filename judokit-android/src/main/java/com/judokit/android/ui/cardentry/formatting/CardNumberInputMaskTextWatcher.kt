50b7d07bc554638512232a8224f1541f857ea275
marcghelas <marc.ghelas@endava.com>
2020-06-11 15:53:27 +0300

    -extract to extension functions

--- judokit-android/src/main/java/com/judokit/android/ui/cardentry/formatting/CardNumberInputMaskTextWatcher.kt
+++ judokit-android/src/main/java/com/judokit/android/ui/cardentry/formatting/CardNumberInputMaskTextWatcher.kt
@@ -6,15 +6,15 @@ import com.judokit.android.model.CardNetwork
 import com.judokit.android.model.CardNetwork.Companion.DEFAULT_CARD_NUMBER_MASK
 import com.judokit.android.model.cardNumberMask
 import com.judokit.android.model.iconImageResId
-import com.judokit.android.model.securityCodeName
-import com.judokit.android.model.securityCodeNumberMask
+import com.judokit.android.model.securityCodeNameOfCardNetwork
+import com.judokit.android.model.securityCodeNumberMaskOfCardNetwork
 import com.judokit.android.parentOfType
 import com.judokit.android.ui.cardentry.components.JudoEditTextInputLayout
 
 internal class CardNumberInputMaskTextWatcher(
     private val editText: EditText,
-    private val securityCodeMask: SecurityCodeInputMaskTextWatcher,
-    private val cardNetwork: CardNetwork? = null
+    private val securityCodeMask: SecurityCodeInputMaskTextWatcher?,
+    private val selectedCardNetwork: CardNetwork? = null
 ) : InputMaskTextWatcher(editText, DEFAULT_CARD_NUMBER_MASK) {
 
     override fun afterTextChanged(s: Editable?) {
@@ -27,14 +27,15 @@ internal class CardNumberInputMaskTextWatcher(
         super.afterTextChanged(s)
     }
 
-    private fun setCardNetworkLogo(network: CardNetwork?) = with(editText) {
-        val resId = network?.iconImageResId ?: 0
+    private fun setCardNetworkLogo(network: CardNetwork) = with(editText) {
+        val resId = network.iconImageResId
         val layout = parentOfType(JudoEditTextInputLayout::class.java)
         layout?.accessoryImage = resId
 
+        val myNetwork = selectedCardNetwork ?: network
         securityCodeMask.apply {
-            hint = cardNetwork?.securityCodeName ?: network?.securityCodeName ?: "CVV"
-            mask = cardNetwork?.securityCodeNumberMask ?: network?.securityCodeNumberMask ?: "###"
+            this?.hint = myNetwork.securityCodeNameOfCardNetwork
+            this?.mask = myNetwork.securityCodeNumberMaskOfCardNetwork
         }
     }
 }
