e1853308ce34e2f842277576e1ac2311cba5a0ce
Stefan Popa <stefan.popa@judopayments.com>
2023-02-21 11:49:48 +0200

    Package and 3DS2 SDK version bump

--- judokit-android/src/main/java/com/judopay/judokit/android/ui/common/Constants.kt
+++ judokit-android/src/main/java/com/judopay/judokit/android/ui/common/Constants.kt
@@ -21,7 +21,7 @@ const val PBBA_RESULT = "PBBA_RESULT"
 /**
  *  Constant to store the sdk version, injected in every Judo API request headers/body
  */
-const val JUDO_KIT_VERSION = "4.0.0"
+const val JUDO_KIT_VERSION = "4.0.1"
 
 /**
  *  Constant to store the Judo API version, injected in every Judo API request headers
