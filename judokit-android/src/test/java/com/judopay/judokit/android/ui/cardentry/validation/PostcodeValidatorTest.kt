e83fa6bf46c1902a634eff631371295d3f020d17
Zeno Foltin <zeno.foltin@gmail.com>
2023-03-26 22:09:42 +0100

    CT-2664 Update postcode validation - tests (#192)
    * Add more tests to improve coverage
    
    * Fixing display name
    
    ---------
    
    Co-authored-by: Stefan Popa <stefan.popa@judopayments.com>

--- judokit-android/src/test/java/com/judopay/judokit/android/ui/cardentry/validation/PostcodeValidatorTest.kt
+++ judokit-android/src/test/java/com/judopay/judokit/android/ui/cardentry/validation/PostcodeValidatorTest.kt
@@ -22,11 +22,41 @@ internal class PostcodeValidatorTest {
     }
 
     @Test
+    @DisplayName("Given the country selected is GB and postcode is too short, then a validation error should be returned with invalid postcode entered string")
+    fun validateTooShortGBPostCode() {
+        validator.country = Country.GB
+        assertEquals(
+            validator.validate("SW1Z", FormFieldEvent.FOCUS_CHANGED),
+            ValidationResult(false, R.string.invalid_postcode)
+        )
+    }
+
+    @Test
+    @DisplayName("Given the country selected is GB and postcode is too long, then a validation error should be returned with invalid postcode entered string")
+    fun validateTooLongGBPostCode() {
+        validator.country = Country.GB
+        assertEquals(
+            validator.validate("SW1Z 1EE ", FormFieldEvent.FOCUS_CHANGED),
+            ValidationResult(false, R.string.invalid_postcode)
+        )
+    }
+
+    @Test
     @DisplayName("Given the country selected is GB and postcode is valid, then validation passes")
     fun invalidateGBPostCode() {
         validator.country = Country.GB
         assertEquals(
             validator.validate("SW15 5PU"),
+            ValidationResult(true, R.string.empty)
+        )
+    }
+
+    @Test
+    @DisplayName("Given the country selected is GB and postcode is valid on focus change, then validation passes")
+    fun validateGBPostCodeOnFocusChange() {
+        validator.country = Country.GB
+        assertEquals(
+            validator.validate("SW15 5PU", FormFieldEvent.FOCUS_CHANGED),
             ValidationResult(true, R.string.empty)
         )
     }
@@ -41,18 +71,35 @@ internal class PostcodeValidatorTest {
         )
     }
 
-    /**
-     * TODO: Fix postcode validation
-     @Test
-     @DisplayName("validate post code for CA")
-     fun validateCAPostCode() {
-     validator.country = Country.CA
-     assertEquals(
-     validator.validate("A1A 1A1"),
-     ValidationResult(true, R.string.empty)
-     )
-     }
-     */
+    @Test
+    @DisplayName("Given the country selected is CA and postcode is too short, then a validation error should be returned with invalid postcode entered string")
+    fun validateTooShortCAPostCode() {
+        validator.country = Country.CA
+        assertEquals(
+            validator.validate("A1A1A", FormFieldEvent.FOCUS_CHANGED),
+            ValidationResult(false, R.string.invalid_postcode)
+        )
+    }
+
+    @Test
+    @DisplayName("Given the country selected is CA and postcode is too long, then a validation error should be returned with invalid postcode entered string")
+    fun validateTooLongCAPostCode() {
+        validator.country = Country.CA
+        assertEquals(
+            validator.validate("A1A 1A1 ", FormFieldEvent.FOCUS_CHANGED),
+            ValidationResult(false, R.string.invalid_postcode)
+        )
+    }
+
+    @Test
+    @DisplayName("Given the country selected is CA and zipcode is valid, then validation passes")
+    fun validateCAPostCode() {
+        validator.country = Country.CA
+        assertEquals(
+            validator.validate("A1A 1A1"),
+            ValidationResult(true, R.string.empty)
+        )
+    }
 
     @Test
     @DisplayName("Given the country selected is US and zipcode is invalid, then a validation error should be returned with invalid postcode entered string")
@@ -60,6 +107,26 @@ internal class PostcodeValidatorTest {
         validator.country = Country.US
         assertEquals(
             validator.validate("zipcode", FormFieldEvent.FOCUS_CHANGED),
+            ValidationResult(false, R.string.invalid_zip_code)
+        )
+    }
+
+    @Test
+    @DisplayName("Given the country selected is US and postcode is too short, then a validation error should be returned with invalid postcode entered string")
+    fun validateTooShortUSPostCode() {
+        validator.country = Country.US
+        assertEquals(
+            validator.validate("1234", FormFieldEvent.FOCUS_CHANGED),
+            ValidationResult(false, R.string.invalid_zip_code)
+        )
+    }
+
+    @Test
+    @DisplayName("Given the country selected is US and postcode is too long, then a validation error should be returned with invalid postcode entered string")
+    fun validateTooLongUSPostCode() {
+        validator.country = Country.US
+        assertEquals(
+            validator.validate("12345-1234 ", FormFieldEvent.FOCUS_CHANGED),
             ValidationResult(false, R.string.invalid_zip_code)
         )
     }
@@ -91,6 +158,16 @@ internal class PostcodeValidatorTest {
         assertEquals(
             validator.validate("postcode"),
             ValidationResult(true, R.string.empty)
+        )
+    }
+
+    @Test
+    @DisplayName("Given the country selected is OTHER and postcode is too long, then a validation error should be returned with invalid postcode entered string")
+    fun validateTooLongOtherPostCode() {
+        validator.country = Country.OTHER
+        assertEquals(
+            validator.validate("12345678901234567", FormFieldEvent.FOCUS_CHANGED),
+            ValidationResult(false, R.string.invalid_postcode)
         )
     }
 
