629fef01e97d9ed7bbda2ebf1a90c183f93ec177
RafalOzogJudopay <rafal.ozog@judopay.com>
2023-07-30 19:29:55 +0100

    CT-2750 4-digit phone code parsing implemented - test fix

--- judokit-android/src/test/java/com/judopay/judokit/android/api/model/request/PaymentRequestTest.kt
+++ judokit-android/src/test/java/com/judopay/judokit/android/api/model/request/PaymentRequestTest.kt
@@ -13,7 +13,7 @@ internal class PaymentRequestTest {
         .setExpiryDate("1220")
         .setSaveCardOnly("123")
         .setEmailAddress("email@mail.com")
-        .setMobileNumber(null, "1234567")
+        .setMobileNumber("1234", "1234567")
 
     @Test
     @DisplayName("Should throw exception on providing null judo id")
