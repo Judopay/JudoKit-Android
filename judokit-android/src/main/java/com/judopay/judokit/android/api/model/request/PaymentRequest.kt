b676bfad79e882ff08b2695e8c4eea735250e831
RafalOzogJudopay <rafal.ozog@judopay.com>
2023-07-31 12:34:07 +0100

    CT-2750 | Cosmetic adjustments to satisfy Lint

--- judokit-android/src/main/java/com/judopay/judokit/android/api/model/request/PaymentRequest.kt
+++ judokit-android/src/main/java/com/judopay/judokit/android/api/model/request/PaymentRequest.kt
@@ -128,15 +128,13 @@ class PaymentRequest private constructor(
             val filteredPhoneCountryCode = phoneCountryCode?.filter { it.isDigit() }
             if (filteredMobileNumber != null) {
                 this.phoneCountryCode =
-                    if (filteredPhoneCountryCode != null && filteredPhoneCountryCode.length > 3)
+                    if (filteredPhoneCountryCode != null && filteredPhoneCountryCode.length > 3) {
                         filteredPhoneCountryCode.substring(0, 1)
-                    else
-                        filteredPhoneCountryCode
+                    } else filteredPhoneCountryCode
                 this.mobileNumber =
-                    if (filteredPhoneCountryCode != null && filteredPhoneCountryCode.length > 3)
+                    if (filteredPhoneCountryCode != null && filteredPhoneCountryCode.length > 3) {
                         filteredPhoneCountryCode.substring(1, 4) + filteredMobileNumber
-                    else
-                        filteredMobileNumber
+                    } else filteredMobileNumber
             }
 
             return PaymentRequest(
