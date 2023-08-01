d95dc0f5a549961d5d533eb3fe7d226629bab3ae
RafalOzogJudopay <138582109+RafalOzogJudopay@users.noreply.github.com>
2023-08-01 10:58:34 +0100

    Update judokit-android/src/main/java/com/judopay/judokit/android/api/model/request/PaymentRequest.kt
    Co-authored-by: Stefan <stefan.popa@judopayments.com>

--- judokit-android/src/main/java/com/judopay/judokit/android/api/model/request/PaymentRequest.kt
+++ judokit-android/src/main/java/com/judopay/judokit/android/api/model/request/PaymentRequest.kt
@@ -124,6 +124,45 @@ class PaymentRequest private constructor(
             //
             // For example, when: dialCode = "1(345)", mobileNumber = "123456"
             // The following is sent to BE: phoneCountryCode = "1", mobileNumber = "3451234567"
+            var filteredMobileNumber = mobileNumber?.filter { it.isDigit() }
+            var filteredPhoneCountryCode = phoneCountryCode?.filter { it.isDigit() }
+
+            if (filteredMobileNumber != null && filteredPhoneCountryCode != null && filteredPhoneCountryCode.length > 3) {
+                val code = filteredPhoneCountryCode.substring(0, 1)
+                val rest = filteredPhoneCountryCode.substring(1, filteredPhoneCountryCode.length)
+
+                filteredPhoneCountryCode = code
+                filteredMobileNumber = rest + filteredMobileNumber
+            }
+
+            return PaymentRequest(
+                uniqueRequest,
+                paymentReference,
+                myAmount,
+                myCurrency,
+                id,
+                consumerReference,
+                yourPaymentMetaData,
+                address,
+                myCardNumber,
+                myCv2,
+                myExpiryDate,
+                startDate,
+                issueNumber,
+                saveCardOnly,
+                emailAddress,
+                filteredMobileNumber,
+                filteredPhoneCountryCode,
+                primaryAccountDetails,
+                initialRecurringPayment,
+                myThreeDSecure,
+                cardHolderName
+            )
+            // Therefore logic has been added to re format dial codes of length 4 (which are always
+            // of format: 1(XXX)), when sending to BE.
+            //
+            // For example, when: dialCode = "1(345)", mobileNumber = "123456"
+            // The following is sent to BE: phoneCountryCode = "1", mobileNumber = "3451234567"
             val filteredMobileNumber = mobileNumber?.filter { it.isDigit() }
             val filteredPhoneCountryCode = phoneCountryCode?.filter { it.isDigit() }
             if (filteredMobileNumber != null) {
