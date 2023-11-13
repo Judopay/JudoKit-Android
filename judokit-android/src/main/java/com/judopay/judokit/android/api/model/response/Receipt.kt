15a14191484d1d578d2bde37daaa46e15eb751f3
Stefan <stefan.popa@judopayments.com>
2023-11-13 12:16:36 +0200

    Ignore char case in `isThreeDSecureTwoRequired` and `isSoftDeclined` for consistency with iOS implementation (#225)
    Co-authored-by: Stefan Popa <popa.stefan.tudor@gmail.com>

--- judokit-android/src/main/java/com/judopay/judokit/android/api/model/response/Receipt.kt
+++ judokit-android/src/main/java/com/judopay/judokit/android/api/model/response/Receipt.kt
@@ -52,10 +52,10 @@ class Receipt(
     val cReq: String? = null
 ) {
     val isThreeDSecureTwoRequired: Boolean
-        get() = message == CHALLENGE_REQUIRED_MESSAGE
+        get() = message.equals(CHALLENGE_REQUIRED_MESSAGE, true)
 
     val isSoftDeclined: Boolean
-        get() = result == DECLINED_RESULT && message == SOFT_DECLINED_MESSAGE && !receiptId.isNullOrEmpty()
+        get() = result.equals(DECLINED_RESULT, true) && message.equals(SOFT_DECLINED_MESSAGE, true) && !receiptId.isNullOrEmpty()
 
     override fun toString(): String {
         return "Receipt(judoId=$judoId, receiptId=$receiptId, originalReceiptId=$originalReceiptId, partnerServiceFee=$partnerServiceFee, yourPaymentReference=$yourPaymentReference, type=$type, createdAt=$createdAt, merchantName=$merchantName, appearsOnStatementAs=$appearsOnStatementAs, originalAmount=$originalAmount, netAmount=$netAmount, amount=$amount, currency=$currency, cardDetails=$cardDetails, consumer=$consumer, risks=$risks, md=$md, paReq=$paReq, acsUrl=$acsUrl, result=$result, message=$message)"
