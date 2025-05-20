196047471cf6064734d1683fe0a84a4cfb3de4a9
Stefan <stefan.popa@judopayments.com>
2025-05-20 12:26:26 +0300

    Migration from OSSRH to Central Portal (#298)
    * Nexus publish plugin version update, AGP update
    * Updates Sonatype URLs
    * ENV variables name updates
    * Fix variable names
    
    ---------
    
    Co-authored-by: Stefan Popa <popa.stefan.tudor@gmail.com>
    Co-authored-by: dave-pollock <david.pollock@judopayments.com>

--- build-logic/convention/src/main/kotlin/com/judopay/judokit/LibraryNexusPublishConfiguration.kt
+++ build-logic/convention/src/main/kotlin/com/judopay/judokit/LibraryNexusPublishConfiguration.kt
@@ -7,7 +7,7 @@ internal fun Project.configureNexusPublish(
     extension: NexusPublishExtension,
 ) = extension.apply {
     with(repositories.sonatype()) {
-        nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/"))
-        snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+        nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
+        snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
     }
 }
