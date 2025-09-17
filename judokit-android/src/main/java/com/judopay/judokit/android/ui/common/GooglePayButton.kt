9cf94afee73c8b19f5d86b10bab170037270c51f
Rafal Ozog <rafal.ozog@judopay.com>
2025-09-17 13:42:27 +0100

    FIxes Google Pay button
    Revert "FIxes Google Pay button"
    
    This reverts commit c43d7fae57b9b796649d1fe2bf5da5a0bc4da9d0.
    
    Fixes Google Pay button

--- judokit-android/src/main/java/com/judopay/judokit/android/ui/common/GooglePayButton.kt
+++ judokit-android/src/main/java/com/judopay/judokit/android/ui/common/GooglePayButton.kt
@@ -58,5 +58,17 @@ class GooglePayButton
 
             binding.whiteBuyWithButton.root.visibility = visibility(GooglePayButtonStyle.WHITE_BUY_WITH)
             binding.whiteBuyWithButtonNoShadow.root.visibility = visibility(GooglePayButtonStyle.WHITE_BUY_WITH_NO_SHADOW)
+
+
+            // No-shadow styles have extra padding in their PNG assets, so we apply a negative margin to match PAY NOW button height.
+            if (style != GooglePayButtonStyle.WHITE_NO_SHADOW && style != GooglePayButtonStyle.WHITE_BUY_WITH_NO_SHADOW) {
+                setMargin(binding.googlePayButtonContainer, -dpToPx(3))
+            }
         }
+
+        private fun setMargin(view: View, px: Int) {
+            (view.layoutParams as? MarginLayoutParams)?.setMargins(px, px, px, px)
+        }
+
+        private fun dpToPx(dp: Int) = (dp * resources.displayMetrics.density + 0.5f).toInt()
     }
