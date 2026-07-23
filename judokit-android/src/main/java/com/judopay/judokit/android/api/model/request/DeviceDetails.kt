d92f12fc33098c8d3d403b87cf6d243fab6489c5
Stefan <stefan.popa@judopayments.com>
2026-07-23 12:49:39 +0300

    Replaces DeviceDNA client details with SDK-native deviceDetails payload (#356)

---
+++ judokit-android/src/main/java/com/judopay/judokit/android/api/model/request/DeviceDetails.kt
@@ -0,0 +1,9 @@
+package com.judopay.judokit.android.api.model.request
+
+internal data class DeviceDetails(
+    val kDeviceId: String?,
+    val vDeviceId: String?,
+    val countryCode: String?,
+    val cultureLocale: String?,
+    val os: String?,
+)
